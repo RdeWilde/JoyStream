01713bf1dc0b3f7a119de0161cb21f0e283ff8a4
Bedeho Mender <bedeho.mender@gmail.com>
2014-10-30 15:00:24 +0100

    Builds properly now

--- QtBitSwapr.pro
+++ QtBitSwapr.pro
@@ -11,10 +11,30 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = QtBitSwapr
 TEMPLATE = app
 
-
-SOURCES += main.cpp\
-        mainwindow.cpp
+SOURCES +=  main.cpp\
+            mainwindow.cpp\
+            ../BitSwapr/core/src/ConsoleView.cpp \
+            ../BitSwapr/core/src/Controller.cpp \
+            ../BitSwapr/core/src/ControllerState.cpp \
+            ../BitSwapr/core/src/ViewRequestCallbackHandler.cpp \
+            ../BitSwapr/core/src/Exceptions/InvalidBitSwaprStateEntryException.cpp \
+            ../BitSwapr/core/src/Exceptions/ListenOnException.cpp \
+            ../BitSwapr/core/src/Exceptions/MissingInfoHashViewRequestException.cpp
 
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+INCLUDEPATH += C:/boost_1_56_0
+INCLUDEPATH += C:/libtorrent-rasterbar-1.0.2/include
+INCLUDEPATH += C:/Users/bedeho/Documents/GitHub/BitSwapr/core/include
+
+DEFINES += WIN32
+DEFINES += NDEBUG
+DEFINES += BOOST_ASIO_SEPARATE_COMPILATION
+DEFINES += TORRENT_DISABLE_GEO_IP
+DEFINES += TORRENT_NO_DEPRECATE
+
+LIBS += -LC:/boost_1_56_0/stage/lib
+LIBS += -LC:/libtorrent-rasterbar-1.0.2/bin/msvc-11.0/release/boost-source/deprecated-functions-off/link-static/threading-multi/ -llibtorrent
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,139 @@
+
+
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+#include <iostream>
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+#include <BitSwapr/Controller.hpp>
+#include <BitSwapr/ControllerState.hpp>
+#include <BitSwapr/ConsoleView.hpp>
+#include <BitSwapr/Exceptions/ListenOnException.hpp>
+
+// Forward declarations
+void loadJVM();
+bool updateManager();
+
+// Name space abbreviations
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+void main(int argc, char* argv[]) {
+
+    // Load JVM
+    loadJVM();
+
+    // Create options description
+    po::options_description desc("Allowed options");
+
+    // Add options
+    desc.add_options()
+      ("help,h", "Print help messages")
+      ("console,c", "Run in console mode")
+      ("fresh,f", "Create and use a fresh parameter file")
+      ("no-update,n", "Do not run update manager");
+
+    // Parse the command line catching and displaying any parser errors
+    po::variables_map vm;
+
+    try {
+
+        // Parse
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+
+        // Throw exception if required option is not present
+        po::notify(vm);
+
+    } catch(po::error& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+        std::cerr << desc << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Display help text when requested
+    if(vm.count("help"))
+        std::cout << desc << std::endl;
+
+    // Call update manager, if allowed
+    if(!vm.count("no-update")) {
+
+        bool doRestart = updateManager();
+
+        if(doRestart) {
+            // Call update runner
+        }
+    }
+
+    // Check console flag
+    bool useConsole = false;
+    if(!vm.count("console"))
+        useConsole = true;
+
+    // Open existing parameter file, or create a fresh one if user passed fresh flag
+    ControllerState * state = NULL;
+    if(!vm.count("fresh")) {
+
+        // Get name of file name
+        fs::path file = operator/(fs::current_path(), fs::path("parameter-file.txt")); // Parameters.DEFAULT_FILE_NAME
+
+        // Check that file exists, and that it actually is a file
+        if(!fs::exists(file)) {
+            std::cerr << "ERROR: parameter file " << file << " does not exist." << std::endl << "Try fresh run option if problem persists" << std::endl << std::endl;
+            exit(EXIT_FAILURE);
+        } else if(!fs::is_regular_file(file)) {
+            std::cerr << "ERROR: parameter file " << file << " is not regular." << std::endl << "Try fresh run option if problem persists" << std::endl << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        // Load state from file
+        state = new ControllerState(file.string().c_str());
+    } else // Load default state
+        state = new ControllerState();
+
+    // Create view
+    QApplication a(argc, argv);
+    MainWindow view;
+
+    // Create controller
+    Controller * controller;
+
+    try {
+        controller = new Controller(*state, &view);
+    } catch(ListenOnException & e) {
+        std::cerr << "ERROR: failed to start libtorrent listening due to " <<  e.what() << "." << std::endl << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Start console ui
+    view.startView(controller);
+    view.show();
+
+    // Start running Qt event loop
+    a.exec();
+
+    // Start servicing session
+    //controller->sessionLoop();
+}
+
+void loadJVM() {
+
+}
+
+bool updateManager() {
+
+    // If we have just downloaded a new binary, then run it and then quit
+
+    // Query blockchain or most recent location
+
+    // Connect to location and check what version is there
+
+    // If its newer, download it to particular location
+
+    // then return to restart:
+
+    return true;
 }
