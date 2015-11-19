cf7371c5257d7b6170609ff94b89f1b2bbf7b168
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-11-19 10:05:57 +0200

    added (c) notice

--- app/AutoUpdater.cpp
+++ app/AutoUpdater.cpp
@@ -1,3 +1,9 @@
+/* Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Mokhtar Naamani <mokhtar.naamani@gmail.com>, September 2015
+ */
+
 #include <AutoUpdater.hpp>
 #include <QDesktopServices>
 #include <QUrl>
