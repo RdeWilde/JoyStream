8ba48f0eae627096bd80ed4c2ead0617c3975f59
Perrom <root@local.host>
2016-01-18 12:05:45 +0100

    TXInput class expanded

--- blockcypher/include/blockcypher/TXInput.hpp
+++ blockcypher/include/blockcypher/TXInput.hpp
@@ -10,6 +10,10 @@
 
 #include <blockcypher/ScriptType.hpp>
 #include <stdutils/uchar_vector.h>
+#include <QString>
+#include <QStringList>
+#include <QJsonValue>
+
 
 class QJsonObject;
 
@@ -30,25 +34,64 @@ namespace BlockCypher {
     */
     struct TXInput {
 
+        // The previous transaction hash where this input was an output. 
+        // Not present for coinbase transactions
+        QString _prev_hash;
+
+        // The index of the output being spent within the previous transaction. 
+        // Not present for coinbase transactions.
+        int _output_index;
+
         // The value of the output being spent within the previous transaction.
         // **Not present for coinbase transactions**
-        uint64_t _output_value;
+        // => BC output_value  integer
+        //uint64_t _output_value;
+        quint64 _output_value;
 
-        // from API: Raw hexadecimal encoding of the encumbrance script for this output.
+        // from API: Raw hexadecimal encoding of the script
+        // =>BC script string
         uchar_vector _script;
 
         // The type of script that encumbers the output corresponding to this input.
+        // =>BC script_type string
         ScriptType _script_type;
 
+        // An array of public addresses associated with the output of the previous transaction.
+        //TODO: This is ok for now, but in the future we likely should use a proper
+        //TODO: polymorhpic address type, as described in issue 65 in the main repo.
+        //TODO: It will encapsulate the different types of address which exist.
+        QStringList _addresses;
+
+        // Legacy 4-byte sequence number, not usually relevant unless dealing with locktime encumbrances
+        uint32_t  _sequence;
+
+        // == Optional fields, are any of these needed?
+        //
+        // Number of confirmations of the previous transaction for which this 
+        // input was an output. Currently, only returned in unconfirmed transactions.
+        // Do we need this?
+        // int age;
+        //
+        // Name of Wallet or HDWallet from which to derive inputs. 
+        // Only used when constructing transactions via the Creating Transactions process.
+        // Do we need this?
+        // QString wallet_name 
+        //
+        // Token associated with Wallet or HDWallet used to derive inputs. 
+        // Only used when constructing transactions via the Creating Transactions process.
+        // QString wallet_token
+
+        // ==
+
+        //Methods takes a QJsonObject as param.
         TXInput(const QJsonObject & o);
-
+        //TXInput(const QJsonValue & o);
+        //Compare operator for this struct.
         bool operator==(const TXInput & o);
-
         Coin::TxIn toInput() const;
-
     };
-
 }
+
 
 #endif // BLOCKCYPHER_TXINPUT_HPP
 
--- blockcypher/src/TXInput.cpp
+++ blockcypher/src/TXInput.cpp
@@ -6,18 +6,58 @@
  */
 
 #include <blockcypher/TXInput.hpp>
-#include <CoinCore/CoinNodeData.h> // TxIn
+#include <CoinCore/CoinNodeData.h> // TxIn 
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QDebug>
+
 
 namespace BlockCypher {
 
 TXInput::TXInput(const QJsonObject & o) {
-    throw std::runtime_error("not done");
+
+        _prev_hash = o.value("prev_hash").toString();
+        _output_index = o.value("output_index").toInt();
+        _output_value = (quint64)o.value("output_value").toDouble();
+
+        //Raw hexadecimal encoding of the script
+        std::string t2 = (o.value("script").toString()).toUtf8().constData();
+        _script = uchar_vector(t2);
+        _script_type = toScriptType(o.value("script_type").toString());
+
+        QJsonArray arr = o.value(("addresses")).toArray();
+
+        foreach (const QJsonValue & value, arr) {
+            QString t = value.toString();
+            _addresses.append(t);
+        }
+
+        _sequence = (quint32)o.value("sequence").toDouble();
+
+        //For debugging purposes.
+        qDebug() << "\nTXInput object: " << o;
+        qDebug() << "TXInput, Inserted _prev_hash:" << _prev_hash;
+        qDebug() << "TXInput, Inserted _output_index:" << _output_index;
+        qDebug() << "TXInput, Inserted _output_value:" << _output_value;
+        qDebug() << "TXInput, Inserted _script:" << QString::fromUtf8(_script.getHex().c_str());
+        qDebug() << "TXInput, Inserted _script_type:" << fromScriptType(_script_type);
+        qDebug() << "TXInput, Inserted _addresses:" << _addresses;
+        qDebug() << "TXInput, Inserted _sequence:" << _sequence;
+
 }
 
 bool TXInput::operator==(const TXInput & o) {
-    return _output_value == o._output_value &&
-            _script == o._script;
-    // no need to check last one
+     return _prev_hash == o._prev_hash &&
+            _output_index == o._output_index &&
+            _output_value == o._output_value &&
+            _script == o._script &&
+            _script_type == o._script_type &&
+            _addresses == o._addresses &&
+            _sequence == o._sequence;
+        // no need to check last one
+
 }
 
 Coin::TxIn TXInput::toInput() const {
