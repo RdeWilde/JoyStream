414e0a31b1cb745ed51abbe379e8afa629a1d20f
Bedeho Mender <bedeho.mender@gmail.com>
2016-01-26 13:41:54 +0100

    TXInput completed

--- blockcypher/include/blockcypher/TXInput.hpp
+++ blockcypher/include/blockcypher/TXInput.hpp
@@ -9,11 +9,10 @@
 #define BLOCKCYPHER_TXINPUT_HPP
 
 #include <blockcypher/ScriptType.hpp>
-#include <stdutils/uchar_vector.h>
+
 #include <QString>
 #include <QStringList>
 #include <QJsonValue>
-
 
 class QJsonObject;
 
@@ -34,6 +33,12 @@ namespace BlockCypher {
     */
     struct TXInput {
 
+        TXInput(const QJsonObject & o);
+
+        bool operator==(const TXInput & o);
+
+        Coin::TxIn toInput() const;
+
         // The previous transaction hash where this input was an output. 
         // Not present for coinbase transactions
         QString _prev_hash;
@@ -43,52 +48,20 @@ namespace BlockCypher {
         int _output_index;
 
         // The value of the output being spent within the previous transaction.
-        // **Not present for coinbase transactions**
-        // => BC output_value  integer
-        //uint64_t _output_value;
+        // ***Not present for coinbase transactions***
         quint64 _output_value;
 
-        // from API: Raw hexadecimal encoding of the script
-        // =>BC script string
-        uchar_vector _script;
+        // Raw hexadecimal encoding of the script
+        QString _script;
 
         // The type of script that encumbers the output corresponding to this input.
-        // =>BC script_type string
         ScriptType _script_type;
 
         // An array of public addresses associated with the output of the previous transaction.
-        //TODO: This is ok for now, but in the future we likely should use a proper
-        //TODO: polymorhpic address type, as described in issue 65 in the main repo.
-        //TODO: It will encapsulate the different types of address which exist.
         QStringList _addresses;
 
         // Legacy 4-byte sequence number, not usually relevant unless dealing with locktime encumbrances
         uint32_t  _sequence;
-
-        // == Optional fields, are any of these needed?
-        //
-        // Number of confirmations of the previous transaction for which this 
-        // input was an output. Currently, only returned in unconfirmed transactions.
-        // Do we need this?
-        // int age;
-        //
-        // Name of Wallet or HDWallet from which to derive inputs. 
-        // Only used when constructing transactions via the Creating Transactions process.
-        // Do we need this?
-        // QString wallet_name 
-        //
-        // Token associated with Wallet or HDWallet used to derive inputs. 
-        // Only used when constructing transactions via the Creating Transactions process.
-        // QString wallet_token
-
-        // ==
-
-        //Methods takes a QJsonObject as param.
-        TXInput(const QJsonObject & o);
-        //TXInput(const QJsonValue & o);
-        //Compare operator for this struct.
-        bool operator==(const TXInput & o);
-        Coin::TxIn toInput() const;
     };
 }
 
