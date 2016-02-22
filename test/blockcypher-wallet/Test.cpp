6c4e2d9a1088e3425efbdc2423684a62e5af140f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-02-22 13:06:43 +0200

    updating blockcypher tests

--- test/blockcypher-wallet/Test.cpp
+++ test/blockcypher-wallet/Test.cpp
@@ -78,6 +78,8 @@ void Test::fundWallet()
     //_wallet->Open();
     //Coin::P2PKHAddress addr(_wallet->GetReceiveAddress());
     //QVERIFY(_client->fundWalletFromFaucet(addr.toBase58CheckEncoding(), 200));
+    //Coin::P2PKHAddress addr2(_wallet->GetReceiveAddress());
+    //QVERIFY(_client->fundWalletFromFaucet(addr2.toBase58CheckEncoding(), 500));
 }
 
 void Test::getUtxo()
@@ -86,36 +88,55 @@ void Test::getUtxo()
     _wallet->Open();
 
     Coin::P2PKHAddress addr(_wallet->GetReceiveAddress());
+    Coin::P2PKHAddress addr2(_wallet->GetReceiveAddress());
 
     QVERIFY(_wallet->Sync());
 
-    // Paid this address on Jan 19 2016 with two transactions - each sending 100 satoshi
-    // Paid this address on Jan 20 2016 with one transaction - sending 200 satoshi
+    // Paid this address on Feb 19 2016 with two transactions - each sending 100 satoshi
+    // Paid this address on Feb 20 2016 with one transaction - sending 200 satoshi
     // https://live.blockcypher.com/btc-testnet/address/mwqoYY153cUiQezsQiFAacC78rdDFUVJQF/
 
-    // requesting 100 min satoshi should require only one of the outputs
+    // Paid this address on Feb 22nd 2016 - 500 satoshi
+    // https://live.blockcypher.com/btc-testnet/address/mr1H6xbVUky81EWt64kZa8z42NSsrmE27B/
+
+    // requesting 200 satoshi should require only one of the outputs
     // It should return the minimum number of coins
     std::list<Coin::UnspentP2PKHOutput> outputs(_wallet->GetUnspentOutputs(200,0,0));
     QVERIFY(outputs.size() == 1);
 
+    // utxos sorted by value so we should see utxo with highest value
     Coin::UnspentP2PKHOutput utxo1 = *outputs.begin();
-    QVERIFY(utxo1.value() == 200);
+    QVERIFY(utxo1.value() == 500);
 
     // on second request we should get two utxos
-    std::list<Coin::UnspentP2PKHOutput> outputs2(_wallet->GetUnspentOutputs(200,0,0));
+    std::list<Coin::UnspentP2PKHOutput> outputs2(_wallet->GetUnspentOutputs(300,0,0));
     QVERIFY(outputs2.size() == 2);
 
     Coin::UnspentP2PKHOutput utxo2 = *outputs2.begin();
-    Coin::UnspentP2PKHOutput utxo3 = *(outputs2.begin()++);
+    Coin::UnspentP2PKHOutput utxo3 = *(++outputs2.begin());
 
-    QVERIFY(utxo2.value() == 100);
+    QVERIFY(utxo2.value() == 200);
     QVERIFY(utxo3.value() == 100);
 
-    // no more utxos
+    // one remaining utxo with value 100
     std::list<Coin::UnspentP2PKHOutput> outputs3(_wallet->GetUnspentOutputs(100,0,0));
-    QVERIFY(outputs3.size() == 0);
-}
+    QVERIFY(outputs3.size() == 1);
+    QVERIFY((*outputs3.begin()).value() == 100);
 
+    // release the utxo
+    _wallet->ReleaseUnspentOutputs(outputs3);
+
+    // we should be able to get the release utxo
+    std::list<Coin::UnspentP2PKHOutput> outputs4(_wallet->GetUnspentOutputs(100,0,0));
+    QVERIFY(outputs4.size() == 1);
+    _wallet->ReleaseUnspentOutputs(outputs3);
+
+    // the one remaining utxo with value 100 is not enough to meet 101 satoshi min value
+    // so returned set should be empty
+    std::list<Coin::UnspentP2PKHOutput> outputs5(_wallet->GetUnspentOutputs(101,0,0));
+    QVERIFY(outputs5.empty());
+
+}
 
 QTEST_MAIN(Test)
 #include "moc_Test.cpp"
