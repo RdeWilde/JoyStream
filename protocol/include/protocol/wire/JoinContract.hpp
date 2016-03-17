f3744623bc2c098ad52a48a417f7abdeebcbda03
Bedeho Mender <bedeho.mender@gmail.com>
2016-03-17 17:42:07 +0100

    Updated join contract message to use contract invitation

--- protocol/include/protocol/wire/JoinContract.hpp
+++ protocol/include/protocol/wire/JoinContract.hpp
@@ -8,17 +8,37 @@
 #ifndef JOYSTREAM_PROTOCOL_WIRE_JOIN_CONTRACT_HPP
 #define JOYSTREAM_PROTOCOL_WIRE_JOIN_CONTRACT_HPP
 
-#include <protocol/wire/NoPayloadMessage.hpp>
+#include <protocol/wire/ExtendedMessagePayload.hpp>
+#include <protocol/ContractInvitation.hpp>
 
 namespace joystream {
 namespace protocol {
 namespace wire {
 
-    class JoinContract : public NoPayloadMessage {
+    class JoinContract : public ExtendedMessagePayload {
 
     public:
 
-        virtual MessageType messageType() const;
+        JoinContract();
+
+        JoinContract(const ContractInvitation & invitation);
+
+        // Constructor based on raw payload
+        JoinContract(QDataStream & stream);
+
+        // Virtual methods that subclassing messages have to implement
+        MessageType messageType() const;
+        quint32 length() const;
+        void write(QDataStream & stream) const;
+
+        // Getters and setters
+        ContractInvitation invitation() const;
+        void setInvitation(const ContractInvitation & invitation);
+
+    private:
+
+        // Invitation to contract
+        ContractInvitation _invitation;
     };
 
 }
--- protocol/src/wire/JoinContract.cpp
+++ protocol/src/wire/JoinContract.cpp
@@ -12,10 +12,52 @@ namespace joystream {
 namespace protocol {
 namespace wire {
 
+    JoinContract::JoinContract() {
+    }
+
+    JoinContract::JoinContract(const ContractInvitation & invitation)
+        : _invitation(invitation) {
+    }
+
+    JoinContract::JoinContract(QDataStream & stream) {
+
+        // Read in variables
+        quint64 value;
+        Coin::PublicKey contractPk, finalPk;
+
+        // Read
+        stream >> value;
+        Coin::operator >> (stream, contractPk);
+        Coin::operator >> (stream, finalPk);
+
+        // Store in invitation
+        _invitation.setValue(value);
+        _invitation.setContractPk(contractPk);
+        _invitation.setFinalPk(finalPk);
+    }
+
+    quint32 JoinContract::length() const {
+        return sizeof(quint64) + Coin::PublicKey::length() + Coin::PublicKey::length();
+    }
+
+    void JoinContract::write(QDataStream & stream) const {
+
+        stream << _invitation.value();
+        Coin::operator << (stream, _invitation.contractPk());
+        Coin::operator << (stream, _invitation.finalPk());
+    }
+
     MessageType JoinContract::messageType() const {
         return MessageType::join_contract;
     }
 
+    ContractInvitation JoinContract::invitation() const {
+        return _invitation;
+    }
+
+    void JoinContract::setInvitation(const ContractInvitation & invitation) {
+        _invitation = invitation;
+    }
 }
 }
 }
