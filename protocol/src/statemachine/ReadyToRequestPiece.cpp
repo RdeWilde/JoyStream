38425e893bd6cdd7a22e5041367fc59d028f92e2
Bedeho Mender <bedeho.mender@gmail.com>
2016-03-29 15:49:40 +0200

    Added state an event in servicing piece state

---
+++ protocol/src/statemachine/ReadyToRequestPiece.cpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, March 29 2016
+ */
+
+#include <protocol/statemachine/ReadyToRequestPiece.hpp>
+#include <protocol/statemachine/WaitingForFullPiece.hpp>
+#include <protocol/wire/RequestFullPiece.hpp>
+
+namespace joystream {
+namespace protocol {
+namespace statemachine {
+
+    ReadyToRequestPiece::ReadyToRequestPiece() {
+        std::cout << "Entering ReadyToRequestPiece state." << std::endl;
+    }
+
+    sc::result ReadyToRequestPiece::react(const event::RequestPiece & e) {
+
+        std::cout << "Reacting to event::RequestPiece event." << std::endl;
+
+        // Request piece from seller
+        context<CBStateMachine>().sendMessage()(new wire::RequestFullPiece(e.pieceIndex()));
+
+        // Wait for
+        return transit<WaitingForFullPiece>();
+    }
+}
+}
+}
