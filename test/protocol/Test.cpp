a8b78763bb5d949c46b67aa36fdd5760a74729fd
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-08 11:49:16 +0200

    test(statemachine): full coverage of statemachine

--- test/protocol/Test.cpp
+++ test/protocol/Test.cpp
@@ -25,6 +25,11 @@
 #include <protocol/statemachine/event/Joined.hpp>
 #include <protocol/statemachine/event/PieceLoaded.hpp>
 #include <protocol/statemachine/event/UpdateTerms.hpp>
+#include <protocol/statemachine/event/InviteSeller.hpp>
+#include <protocol/statemachine/exception/CannotInviteNonSeller.hpp>
+#include <protocol/statemachine/event/ContractPrepared.hpp>
+#include <protocol/statemachine/event/RequestPiece.hpp>
+#include <protocol/statemachine/event/SendPayment.hpp>
 
 void Test::observing() {
 
@@ -288,9 +293,16 @@ void Test::buyerAndSellers() {
 void Test::buying() {
 
     BuyingNavigator::Fixture f;
-    f.peerTerms = joystream::wire::SellerTerms(0,1,0,1,0);
-    f.clientTerms = joystream::wire::BuyerTerms(4,4,4,4,5);
-
+    f.peerTerms = joystream::wire::SellerTerms(11,22,33,44,55);
+    f.index = 5;
+    f.clientTerms = joystream::wire::BuyerTerms(88,77,66,99,111);
+    f.value = 2222;
+    f.buyerContractKeyPair = Coin::KeyPair(Coin::PrivateKey("153213303DA61F20BD67FC233AA33262"));
+    f.finalPkHash = Coin::PubKeyHash("3457b36d53494fb1ce39a4500d76373da994585e");
+    f.rsvp = joystream::wire::ContractRSVP(Coin::PublicKey(uchar_vector("03ffe71c26651de3056af555d92cee57a42c36976ac1259f0b5cae6b9e94ca38d8")),
+                                           Coin::PubKeyHash("892131b6cbf303692785db2c607fb915ae622203"));
+    f.anchor = Coin::typesafeOutPoint(Coin::TransactionId::fromRPCByteOrder(std::string("eee2b334dd735dac60ae57893c2528087fd3d386b57cac42f4e6ace6403f16b3")), 78);
+    f.pieceData = joystream::wire::PieceData();
 
     BuyingNavigator navigator(f);
 
@@ -298,39 +310,133 @@ void Test::buying() {
     CBStateMachineCallbackSpy spy;
     CBStateMachine * machine = spy.createMonitoredMachine();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-    CBStateMachineCallbackSpy spy;
-
-    // Create machine in ChooseMode state
-    CBStateMachine * machine = spy.createMonitoredMachine();
-
-    joystream::wire::BuyerTerms terms(1,2,3,4,5);
-    machine->process_event(event::BuyModeStarted(terms));
+    // Client changes to buy mode
+    navigator.toBuyMode(machine);
 
     // Check that sell message was sent with correct terms
     QVERIFY(spy.messageSent());
-    QCOMPARE((static_cast<const joystream::wire::Buy *>(spy.message().get()))->terms(), terms);
+    QCOMPARE((static_cast<const joystream::wire::Buy *>(spy.message().get()))->terms(), f.clientTerms);
 
-    //// In Observing state
-    */
+    spy.reset();
+
+    //// All states below are substates of Buying state
+
+    std::cout << "--- In ReadyToInviteSeller state ---" << std::endl;
+
+    // Client, incorrectly, tries to invite peer
+    // which is in buy mode, which should result in exception
+    peerToBuyMode(machine, joystream::wire::BuyerTerms());
+    QVERIFY_EXCEPTION_THROWN(machine->process_event(event::InviteSeller(0, Coin::KeyPair(), Coin::PubKeyHash())),
+                             exception::CannotInviteNonSeller);
+
+    // Recreate fresh machine in sell mode,
+    // the prior exception destroys machine state
+    delete machine;
+    machine = spy.createMonitoredMachine();
+    navigator.toBuyMode(machine);
+    peerToSellMode(machine, f.peerTerms, f.index);
+
+    spy.reset();
+
+    // Invite seller peer
+    machine->process_event(event::InviteSeller(f.value, f.buyerContractKeyPair, f.finalPkHash));
+
+    QVERIFY(spy.messageSent());
+    QCOMPARE((static_cast<const joystream::wire::JoinContract *>(spy.message().get()))->index(), f.index);
+
+    spy.reset();
+
+    std::cout << "--- In WaitingForSellerToJoin state ---" << std::endl;
+
+    // Peer joins contract
+    machine->process_event(event::Recv<joystream::wire::JoiningContract>(new joystream::wire::JoiningContract(f.rsvp)));
+
+    QVERIFY(spy.sellerHasJoined());
+
+    spy.reset();
+
+    std::cout << "--- In PreparingContract state ---" << std::endl;
+
+    // Peer interrupts contract by announcing sell mode
+    peerToSellMode(machine, joystream::wire::SellerTerms(), 0);
+
+    QVERIFY(spy.sellerHasInterruptedContract());
+
+    spy.reset();
+
+    // Peer interrupts contract by announcing buy mode: currently in ReadyToInviteSeller
+    navigator.toSellerHasJoined(machine);
+    spy.reset();
+
+    peerToBuyMode(machine, joystream::wire::BuyerTerms());
+
+    QVERIFY(spy.sellerHasInterruptedContract());
+
+    spy.reset();
+
+    // Peer interrupts contract by announcing observe mode: currently in ReadyToInviteSeller
+    navigator.toSellerHasJoined(machine);
+    spy.reset();
+
+    peerToObserveMode(machine);
+
+    QVERIFY(spy.sellerHasInterruptedContract());
+
+    spy.reset();
+
+    // Client announces contract as ready
+    navigator.toSellerHasJoined(machine);
+    spy.reset();
+
+    machine->process_event(event::ContractPrepared(f.anchor));
+
+    QVERIFY(spy.messageSent());
+    QCOMPARE((static_cast<const joystream::wire::Ready *>(spy.message().get()))->anchor(), f.anchor);
+
+    spy.reset();
+
+    std::cout << "--- In ReadyToRequestPiece state ---" << std::endl;
+
+    // Client requests a piece
+    machine->process_event(event::RequestPiece(f.pieceIndex));
+
+    QVERIFY(spy.messageSent());
+    QCOMPARE((static_cast<const joystream::wire::RequestFullPiece *>(spy.message().get()))->pieceIndex(), f.pieceIndex);
+
+    spy.reset();
+
+    std::cout << "--- In WaitingForFullPiece state ---" << std::endl;
+
+    // Peer sends piece back to client
+    machine->process_event(event::Recv<joystream::wire::FullPiece>(new joystream::wire::FullPiece(f.pieceData)));
+
+    QVERIFY(spy.hasReceivedFullPiece());
+    QCOMPARE(spy.pieceData(), f.pieceData);
+
+    spy.reset();
+
+    std::cout << "--- In ProcessingPiece state ---" << std::endl;
+
+    // Client says piece was valid, and payment is sent
+    machine->process_event(event::SendPayment());
+
+    QVERIFY(spy.messageSent());
+    QVERIFY(navigator.validatePayment((static_cast<const joystream::wire::Payment *>(spy.message().get()))->sig(), 0));
+
+    spy.reset();
+
+    // Update terms
+    joystream::wire::BuyerTerms testTerms(4123,65436,1432,652,77777);
+    machine->process_event(event::UpdateTerms<joystream::wire::BuyerTerms>(testTerms));
+
+    QVERIFY(spy.messageSent());
+    QCOMPARE((static_cast<const joystream::wire::Buy *>(spy.message().get()))->terms(), testTerms);
+
+    spy.reset();
 
     // Clean up machine
     delete machine;
 }
-
 
 void Test::peerToSellMode(CBStateMachine * machine, const joystream::wire::SellerTerms & terms, uint32_t index) {
 
