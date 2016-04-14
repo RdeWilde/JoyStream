bdc0f2fd7b1039eba5eb126e20cd505b2fa36d5c
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-14 11:27:55 +0200

    test(protocol_test): Sell coverage, passing

--- test/protocol_wire/Test.cpp
+++ test/protocol_wire/Test.cpp
@@ -34,5 +34,33 @@ void Test::buy() {
     QCOMPARE(m, m2);
 }
 
+void Test::sell() {
+
+    // Sell(const SellerTerms & terms): construct message
+    SellerTerms terms(2,4,5,6,7);
+    uint32_t index = 44;
+    Sell m(terms, index);
+
+    // Getters
+    QCOMPARE(m.terms(), terms);
+    QCOMPARE(m.index(), index);
+    QCOMPARE(m.messageType(), MessageType::sell);
+
+    // write(QDataStream & stream)
+    QByteArray raw(m.length(), 0);
+    QDataStream writeStream(&raw, QIODevice::WriteOnly);
+
+    m.write(writeStream);
+
+    // Buy(QDataStream & stream): load new message from stream
+    QDataStream readStream(raw);
+    Sell m2(readStream);
+
+    // and compare the two and make sure they are identical
+    QCOMPARE(m, m2);
+}
+
+
+
 QTEST_MAIN(Test)
 #include "moc_Test.cpp"
