230f10c5416b22e8769ba63d4a403bfff63258c2
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-18 15:47:25 +0200

    new(protocol_session): added convenience header

---
+++ protocol_session/include/protocol_session/protocol_session.hpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, April 18 2016
+ */
+
+#ifndef JOYSTREAM_PROTOCOL_SESSION_HPP
+#define JOYSTREAM_PROTOCOL_SESSION_HPP
+
+#include <protocol_session/BuyerSessionState.hpp>
+#include <protocol_session/Buying.hpp>
+#include <protocol_session/Connection.hpp>
+#include <protocol_session/Mode.hpp>
+#include <protocol_session/Piece.hpp>
+#include <protocol_session/Seller.hpp>
+#include <protocol_session/Selling.hpp>
+#include <protocol_session/Session.hpp>
+#include <protocol_session/SessionMode.hpp>
+
+#endif // JOYSTREAM_PROTOCOL_SESSION_HPP
