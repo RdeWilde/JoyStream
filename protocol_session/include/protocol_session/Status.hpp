9975a0dfe1e818c14691397aaa7f923ba53327f4
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-21 17:32:47 +0200

    new(Status): initial sketch of Status types

---
+++ protocol_session/include/protocol_session/Status.hpp
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, April 21 2016
+ */
+
+#ifndef JOYSTREAM_PROTOCOLSESSION_STATUS
+#define JOYSTREAM_PROTOCOLSESSION_STATUS
+
+
+// All implementations are gathered in one file
+// due to: ?
+
+namespace joystream {
+namespace protocol_session {
+namespace status {
+
+    template <class ConnectionIdType>
+    class Payee {
+
+    };
+
+    template <class ConnectionIdType>
+    class Payor {
+
+    };
+
+    template <class ConnectionIdType>
+    class CBStateMachine {
+
+    };
+
+    template <class ConnectionIdType>
+    class Connection {
+
+    };
+
+    template <class ConnectionIdType>
+    class Buying {
+
+    };
+
+    template <class ConnectionIdType>
+    class Selling {
+
+    };
+
+    template <class ConnectionIdType>
+    class Seller {
+
+    };
+
+    template <class ConnectionIdType>
+    class Piece {
+
+    };
+
+    template <class ConnectionIdType>
+    class Session {
+
+    };
+
+}
+}
+}
+
+#endif // JOYSTREAM_PROTOCOLSESSION_STATUS
