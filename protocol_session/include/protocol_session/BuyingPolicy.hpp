cac3d2c632902a776952e677b73a48c1e36bbb59
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-30 18:43:28 +0200

    new(BuyingPolicy): factor out policy for buying state

---
+++ protocol_session/include/protocol_session/BuyingPolicy.hpp
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, April 28 2016
+ */
+
+#ifndef JOYSTREAM_PROTOCOLSESSION_BUYINGPOLICY_HPP
+#define JOYSTREAM_PROTOCOLSESSION_BUYINGPOLICY_HPP
+
+namespace joystream {
+namespace protocol_session {
+
+class BuyingPolicy {
+
+public:
+
+    BuyingPolicy()
+        : _minTimeBeforeBuildingContract(0)
+        , _servicingPieceTimeOutLimit(0) {
+    }
+
+    BuyingPolicy(double minTimeBeforeBuildingContract, double servicingPieceTimeOutLimit)
+        : _minTimeBeforeBuildingContract(minTimeBeforeBuildingContract)
+        , _servicingPieceTimeOutLimit(servicingPieceTimeOutLimit) {
+    }
+
+private:
+
+    // 1
+    // The minimum amount of time (s) required before
+    // trying to buil a contract
+    double _minTimeBeforeBuildingContract;
+
+    // 2
+    // time to wait, at very least,
+
+    // 3
+    // How long to wait before trying to ask someone else for the same piece
+    // if a peer is not responding, or responding too slowly
+
+    // 4
+    // Ranking peers for invite:
+    // if there are more peers than your desired max,
+    // do you pick random subset, or minimize on price.
+
+    // 5
+    // Ranking peers for downloading
+
+    // 6
+    // Ranking piece for download order
+
+    // 7
+    //
+    double _servicingPieceTimeOutLimit;
+
+};
+
+}
+}
+
+#endif // JOYSTREAM_PROTOCOLSESSION_BUYINGPOLICY_HPP
