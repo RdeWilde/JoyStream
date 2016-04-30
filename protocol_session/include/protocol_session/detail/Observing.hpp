a948362d6831656dfdd6e87012b4d310bbc52de9
Bedeho Mender <bedeho.mender@gmail.com>
2016-04-30 18:43:52 +0200

    new(Observing): initial sketch

---
+++ protocol_session/include/protocol_session/detail/Observing.hpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, April 29 2016
+ */
+
+#ifndef JOYSTREAM_PROTOCOLSESSION_OBSERVING_HPP
+#define JOYSTREAM_PROTOCOLSESSION_OBSERVING_HPP
+
+#include <protocol_session/Session.hpp>
+
+namespace joystream {
+namespace protocol_session {
+namespace detail {
+
+template <class ConnectionIdType>
+class Selling;
+
+template <class ConnectionIdType>
+class Buying;
+
+template <class ConnectionIdType>
+class Observing {
+
+public:
+
+    Observing(Session<ConnectionIdType> *);
+
+    //// Manage connections
+
+    // Connection with given id has been removed (ex-post)
+    void removeConnection(const ConnectionIdType &);
+
+    // Process given message on given connection with given ID
+    void processMessageOnConnection(const ConnectionIdType &, const protocol_wire::ExtendedMessagePayload &);
+
+    //// Change mode
+
+    // Turn into session in sell mode
+    // Caller owns returned object.
+    Selling<ConnectionIdType> * toSellMode();
+
+    // Turn into session in observe mode
+    // Caller owns returne object.
+    Buying<ConnectionIdType> * toBuyMode();
+
+private:
+
+    //// Members
+
+    // Reference to core of session
+    Session<ConnectionIdType> * _session;
+
+};
+
+}
+}
+}
+
+// Needed due to c++ needing implementation for all uses of templated types
+#include <protocol_session/../../src/detail/Observing.cpp>
+#endif // JOYSTREAM_PROTOCOLSESSION_OBSERVING_HPP
