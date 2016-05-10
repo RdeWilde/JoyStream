d74d1e7d2edf61cae386ce3cce90d5f9367e4b1e
Bedeho Mender <bedeho.mender@gmail.com>
2016-05-10 08:51:03 +0200

    new(SessionContext): initial work on thread based test of Sessions

---
+++ test/protocol_session/SessionContext.cpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, May 9 2016
+ */
+
+#include "SessionContext.hpp"
