78d11931a9c59c16b51ea1bc221c2996ed4a9247
Bedeho Mender <bedeho.mender@gmail.com>
2016-05-27 06:48:23 +0200

    newTest(protocol_session): basic observe mode coverage

--- test/protocol_session/Test.cpp
+++ test/protocol_session/Test.cpp
@@ -6,25 +6,24 @@
  */
 
 #include <Test.hpp>
-#include <SessionSpy.hpp>
-
-using namespace joystream;
-using namespace joystream::protocol_session;
-
-typedef uint ID;
 
 template<>
 std::string IdToString<ID>(const ID & s) {
     return std::to_string(s);
 }
 
-void Test::session() {
+Test::Test()
+    : network(Coin::Network::testnet3)
+    , session(nullptr)
+    , spy(nullptr) {
+}
 
-    Coin::Network network = Coin::Network::testnet3;
+void Test::initTestCase() {
 
-    Session<ID> session;
+    session = new Session<ID>();
 
-    SessionSpy<ID> spy(
+    spy = new
+    SessionSpy<ID>(
     [this](int n) -> std::vector<Coin::KeyPair> {
 
         std::vector<Coin::KeyPair> keys;
@@ -34,12 +33,12 @@ void Test::session() {
 
         return keys;
     },
-    [this, &network](int n) {
+    [this](int n) {
 
         std::vector<Coin::P2PKHAddress> addresses;
 
         for(int i = 0;i < n;i++)
-            addresses.push_back(Coin::PrivateKey::generate().toPublicKey().toP2PKHAddress(network));
+            addresses.push_back(Coin::PrivateKey::generate().toPublicKey().toP2PKHAddress(this->network));
 
         return addresses;
 
@@ -47,24 +46,318 @@ void Test::session() {
     [this](const Coin::Transaction &) {
         return true;
     },
-    &session);
+    session);
+}
 
-    spy.toMonitoredObserveMode();
+void Test::cleanup() {
+    delete session;
+    delete spy;
+
+    session = nullptr;
+    spy = nullptr;
+}
+
+void Test::observing() {
+
+    // to observe mode
+    toObserveMode();
+
+    // incorrectly try to go to same mode again
+    QVERIFY_EXCEPTION_THROWN(toObserveMode(), exception::SessionAlreadyInThisMode);
 
     // Start session
-    session.start();
+    start();
 
-    // Add connection which is monitored
-    ID c0 = 0;
-    ConnectionSpy<ID> * connectionSpy = spy.addConnection(c0);
+    // incorrectly try to start again
+    QVERIFY_EXCEPTION_THROWN(start(), exception::StateIncompatibleOperation);
 
-    // Verify that mode message sent to peer
-    QVERIFY(connectionSpy->sendMessageOnConnectionCallbackSlot.called);
-    QVERIFY(spy.noSessionEvents()); // but nothing has otherwised happned in the session
-    QVERIFY(spy.noConnectionEventsExcept(c0)); // or on any other connections
+    // Do basic tests
+    basic();
 
-    connectionSpy->reset();
+    // << ---- here we have stopped session ---->
 
+    start();
+
+    // Have some new peers join, without announcing mode,
+    // and transition to buy mode
+    addConnection(0);
+    addConnection(1);
+
+    // Go to buy mode
+    toBuyMode(Coin::UnspentP2PKHOutput(),
+              BuyingPolicy(),
+              protocol_wire::BuyerTerms(),
+              TorrentPieceInformation());
+
+    // back to observe mode
+    toObserveMode();
+
+    // Go to sell mode
+    toSellMode(SellingPolicy(),
+               protocol_wire::SellerTerms());
+
+}
+
+void Test::selling() {
+
+    // same as general stuff as above
+
+    //
+
+    //void toSellMode(Session<ID> * session, SessionSpy<ID> * spy);
+
+
+    // pausing a seller?
+    // if there is a pending request, then make an additional call
+    // on session to load piece. This should result in a final full piece being sent???
+    // NB: ****this makes no sense, as connection will be gone.***
+    // no case for: mode == SessionMode::observing
+    // *** make sure that you deal with this later
+
+
+    // update terms
+
+    /**
+    LoadPieceForBuyerCallbackSlot<ConnectionIdType> loadPieceForBuyerCallbackSlot;
+    ClaimLastPaymentCallbackSlot<ConnectionIdType> claimLastPaymentCallbackSlot;
+    AnchorAnnouncedCallbackSlot<ConnectionIdType> anchorAnnouncedCallbackSlot;
+
+    //// Selling
+
+    // Data for given piece has been loaded
+    void pieceLoaded(const ConnectionIdType &, const protocol_wire::PieceData &, int);
+
+    // Update terms when selling
+    void updateTerms(const protocol_wire::SellerTerms &);
+    */
+}
+
+void Test::buying() {
+
+    //void toBuyMode(Session<ID> * session, SessionSpy<ID> * spy);
+
+    // tick(): right away, and see that invites are sent
+
+    // have some peers send
+    // tick(): check that invites are not sent to peers
+
+
+    /**
+
+      check
+    BroadcastTransactionCallbackSlot broadcastTransactionCallbackSlot;
+    FullPieceArrivedCallbackSlot<ConnectionIdType> fullPieceArrivedCallbackSlot;
+
+
+    //// Buying
+
+    // A valid piece was sent too us on given connection
+    void validPieceReceivedOnConnection(const ConnectionIdType &, int index);
+
+    // An invalid piece was sent too us on given connection
+    void invalidPieceReceivedOnConnection(const ConnectionIdType &, int index);
+
+    // Piece with given index has been downloaded, but not through
+    // a regitered connection. Could be non-joystream peers, or something out of bounds.
+    void pieceDownloaded(int);
+
+    // Update terms
+    void updateTerms(const protocol_wire::BuyerTerms &);
+
+    */
+
+}
+
+void Test::basic() {
+
+    //// (1) four peers join without announcing their mode
+
+    addConnection(0);
+    addConnection(1);
+    addConnection(2);
+    addConnection(3);
+
+    // add same as before
+    QVERIFY_EXCEPTION_THROWN(addConnection(0), joystream::protocol_session::exception::ConnectionAlreadyAddedException<ID>);
+
+    //// (2) one drops out
+
+    removeConnection(3);
+
+    //// (3) the rest change modes
+
+    session->processMessageOnConnection(0, protocol_wire::Observe());
+    session->processMessageOnConnection(1, protocol_wire::Sell(protocol_wire::SellerTerms(1,2,3,4,5), 1337));
+    session->processMessageOnConnection(2, protocol_wire::Buy(protocol_wire::BuyerTerms(10,20,30,40,50)));
+
+    //// (4) pause
+
+    pause();
+
+    //// (5) new peer joins
+
+    addConnection(4);
+
+    //// (6) old peer updates terms
+
+    session->processMessageOnConnection(0, protocol_wire::Observe());
+    session->processMessageOnConnection(1, protocol_wire::Sell(protocol_wire::SellerTerms(33,44,66,22,32), 1338));
+    session->processMessageOnConnection(2, protocol_wire::Buy(protocol_wire::BuyerTerms(100,200,300,400,500)));
+
+    //// (7) stop
+
+    stop();
+}
+
+void Test::addConnection(ID id) {
+
+    ConnectionSpy<ID> * c = spy->addConnection(id);
+
+    verifyTermsSentToPeer(c->sendMessageOnConnectionCallbackSlot);
+
+    // Check the state of session
+    QVERIFY(session->hasConnection(id));
+
+    auto ids = session->connectionIds();
+    auto itr = std::find(ids.cbegin(), ids.cend(), id);
+    QVERIFY(itr != ids.cend());
+
+    QVERIFY(spy->blankSession());
+
+    spy->reset();
+}
+
+void Test::removeConnection(ID id) {
+
+    session->removeConnection(id);
+
+    QVERIFY(!session->hasConnection(id));
+    auto ids = session->connectionIds();
+    auto itr = std::find(ids.cbegin(), ids.cend(), id);
+    QVERIFY(itr == ids.cend());
+    QCOMPARE((int)spy->removedConnectionCallbackSlot.size(), 1);
+
+    auto f = spy->removedConnectionCallbackSlot.front();
+    QCOMPARE(std::get<0>(f), id);
+    QCOMPARE(std::get<1>(f), DisconnectCause::client);
+
+    spy->reset();
+}
+
+void Test::start() {
+
+    session->start();
+
+    QCOMPARE(session->state(), SessionState::started);
+
+    // iterate peers and check that correct mode message had been sent to whatever
+    for(auto mapping : spy->connectionSpies)
+        verifyTermsSentToPeer(mapping.second->sendMessageOnConnectionCallbackSlot);
+
+    spy->reset();
+}
+
+void Test::stop() {
+
+    SessionMode mode = session->mode();
+    assert(mode != SessionMode::not_set);
+
+    // If session is in buying, identify connections
+    // which are in X state, and hence should be given courtsey payment
+    if(mode == SessionMode::buying) {
+
+        // iterate connection c:
+            // if c.inthis state
+                // c.id()
+
+    }
+
+    // Get all connections present before stop
+    std::set<ID> presentBeforeStop = session->connectionIds();
+
+    session->stop();
+
+    QCOMPARE(session->state(), SessionState::stopped);
+
+    // Check that removal callback was made for each
+    std::set<ID> removed;
+    for(auto f : spy->removedConnectionCallbackSlot)
+        removed.insert(std::get<0>(f));
+
+    QCOMPARE(removed, presentBeforeStop);
+
+    // If session is buying, last best effort message was correctly sent
+    if(mode == SessionMode::buying) {
+
+        // if we were in some spesific state (how to check that now),
+        // then check if the connection received a valid last payment
+    }
+
+    // no case for: mode == SessionMode::selling, SessionMode::observing
+
+    spy->reset();
+    spy->removeConnectionSpies();
+}
+
+void Test::pause() {
+
+    session->pause();
+    QCOMPARE(session->state(), SessionState::paused);
+    QVERIFY(spy->blank());
+}
+
+void Test::toObserveMode() {
+
+    spy->toMonitoredObserveMode();
+
+    QCOMPARE(session->mode(), SessionMode::observing);
+}
+
+void Test::toSellMode(const SellingPolicy & policy,
+                      const protocol_wire::SellerTerms & terms) {
+
+    spy->toMonitoredSellMode(policy, terms);
+
+    QCOMPARE(session->mode(), SessionMode::selling);
+}
+
+void Test::toBuyMode(const Coin::UnspentP2PKHOutput & funding,
+                     const BuyingPolicy & policy,
+                     const protocol_wire::BuyerTerms & terms,
+                     const TorrentPieceInformation & information) {
+
+    spy->toMonitoredBuyMode(funding,
+                           policy,
+                           terms,
+                           information);
+
+    QCOMPARE(session->mode(), SessionMode::buying);
+
+
+}
+
+void Test::verifyTermsSentToPeer(const SendMessageOnConnectionCallbackSlot & slot) {
+
+    QCOMPARE((int)slot.size(), 1);
+    auto f = slot.front();
+    const protocol_wire::ExtendedMessagePayload * m = std::get<0>(f);
+
+    switch(session->mode()) {
+        case SessionMode::not_set: assert(false);
+        case SessionMode::buying:
+        QCOMPARE(m->messageType(), protocol_wire::MessageType::buy);
+        //QCOMPARE(session->)
+        break;
+        case SessionMode::selling:
+        QCOMPARE(m->messageType(), protocol_wire::MessageType::sell);
+
+        break;
+        case SessionMode::observing:
+        QCOMPARE(m->messageType(), protocol_wire::MessageType::observe);
+        break;
+        default:
+            assert(false);
+    }
 }
 
 QTEST_MAIN(Test)
--- test/protocol_session/Test.hpp
+++ test/protocol_session/Test.hpp
@@ -10,20 +10,92 @@
 
 #include <QtTest/QtTest>
 
-//using namespace joystream::protocol_session;
+#include <SessionSpy.hpp>
+
+using namespace joystream;
+using namespace joystream::protocol_session;
+
+// Id type used to identify connections
+typedef uint ID;
 
 class Test : public QObject {
 
     Q_OBJECT
 
-private slots:
-
-    void session();
-
-
 public:
 
+    Test();
 
+private slots:
+
+    // Runs before each unit, sets up session and spy
+
+    void initTestCase();
+    void cleanup();
+
+    //// Cases
+
+    //
+    void observing();
+
+    //
+    void selling();
+
+    //
+    void buying();
+
+private:
+
+    // Variable shared across all units tests
+
+    Coin::Network network;
+    Session<ID> * session;
+    SessionSpy<ID> * spy;
+
+    // Generators for predictable keys & addresse &...
+
+    //// Routines for doing spesific set of tests which can be used across number of cases
+    //// NB: None of these routines can return values, as they use QTest macroes which dont return this value.
+    //// NB: Spy is always reset, if affected, by each call
+
+    // Takes started session and spy for session:
+    // (1) four peers join without announcing their mode
+    // (2) one drops out
+    // (3) the rest change modes
+    // (4) pause
+    // (5) new peer joins
+    // (6) old peer updates terms
+    // (7) stop
+    void basic();
+
+    // Adds peer
+    void addConnection(ID);
+
+    // Peer disconnects
+    void removeConnection(ID);
+
+    // Alter session state
+    void start();
+
+    //
+    void stop();
+
+    //
+    void pause();
+
+    // Alter session mode
+    void toObserveMode();
+
+    void toSellMode(const SellingPolicy &,
+                    const protocol_wire::SellerTerms &);
+
+    void toBuyMode(const Coin::UnspentP2PKHOutput &,
+                   const BuyingPolicy &,
+                   const protocol_wire::BuyerTerms &,
+                   const TorrentPieceInformation &);
+
+    // Utilities
+    void verifyTermsSentToPeer(const SendMessageOnConnectionCallbackSlot &);
 };
 
 #endif // TEST_HPP
