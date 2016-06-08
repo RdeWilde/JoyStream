cb9cbe1922a6b651a8ef677f20b9d8d755bfee87
Bedeho Mender <bedeho.mender@gmail.com>
2016-06-08 19:44:32 +0200

    new: alert convenience header

--- extension/include/extension/alert/alert.hpp
+++ extension/include/extension/alert/alert.hpp
@@ -1,5 +1,24 @@
-#ifndef ALERT_HPP
-#define ALERT_HPP
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, June 7 2016
+ */
 
-#endif // ALERT_HPP
+#ifndef JOYSTREAM_EXTENSION_ALERT_HPP
+#define JOYSTREAM_EXTENSION_ALERT_HPP
 
+#include <extension/alert/AlertTypes.hpp>
+#include <extension/alert/BuyerPeerAddedAlert.hpp>
+#include <extension/alert/BuyerPeerPluginRemovedAlert.hpp>
+#include <extension/alert/BuyerTorrentPluginStatusAlert.hpp>
+#include <extension/alert/PluginStatusAlert.hpp>
+#include <extension/alert/SellerPeerAddedAlert.hpp>
+#include <extension/alert/SellerPeerPluginRemovedAlert.hpp>
+#include <extension/alert/SellerTorrentPluginStatusAlert.hpp>
+#include <extension/alert/StartedBuyerTorrentPlugin.hpp>
+#include <extension/alert/StartedSellerTorrentPlugin.hpp>
+#include <extension/alert/TorrentPluginAlert.hpp>
+#include <extension/alert/TorrentPluginStartedAlert.hpp>
+
+#endif // JOYSTREAM_EXTENSION_ALERT_HPP
