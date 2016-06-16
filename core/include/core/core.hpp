f08d45ea6440585294585208f248e7600a55182a
Bedeho Mender <bedeho.mender@gmail.com>
2016-06-16 06:31:00 +0200

    fix(configuration): draft of new classes

---
+++ core/include/core/core.hpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, June 15 2016
+ */
+
+#ifndef JOYSTREAM_CORE_CONTROLLER_HPP
+#define JOYSTREAM_CORE_CONTROLLER_HPP
+
+#include <core/Controller.hpp>
+#include <core/Configuration.hpp>
+
+#endif // JOYSTREAM_CORE_CONTROLLER_HPP
