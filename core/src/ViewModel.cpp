c8aba39275dbadbef9ca317ee50af2a189abfd22
Bedeho Mender <bedeho.mender@gmail.com>
2016-06-16 12:01:02 +0200

    new(core::ViewModel): namespace for new view models

---
+++ core/src/ViewModel.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, June 16 2016
+ */
+
+#include <core/ViewModel.hpp>
+
+namespace joystream {
+namespace core {
+namespace viewmodel {
+
+
+}
+}
+}
