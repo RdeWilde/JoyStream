07e2f522babd54d8c11e07da779b7b77c4adc885
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-06 13:51:25 +0200

    simplified protocol_wire::MessageType

--- protocol_wire/src/MessageType.cpp
+++ protocol_wire/src/MessageType.cpp
@@ -6,16 +6,13 @@
  */
 
 #include <protocol_wire/MessageType.hpp>
-
-#include <QtGlobal> // Q_ASSERT
-
-#include <vector>
+#include <cassert>
 #include <string>
 
 namespace joystream {
 namespace protocol_wire {
 
-    const char * MessageTypeToString(MessageType type) {
+    const char * messageName(MessageType type) {
 
         switch(type){
             case MessageType::observe: return "observe";
@@ -29,89 +26,37 @@ namespace protocol_wire {
             case MessageType::request_full_piece: return "request_full_piece";
             case MessageType::full_piece: return "full_piece";
             case MessageType::payment: return "payment";
-
             default:
-                 Q_ASSERT(false);
+                 assert(false);
         }
     }
 
+    MessageType messageType(const std::string & name) {
 
-    const std::map<MessageType, const char *> messageTypeToNameMapping() {
-
-        // Create type to name mapping
-        std::map<MessageType, const char *> m;
-
-        // Setup mapping
-        m[MessageType::observe] = "observe";
-        m[MessageType::buy] = "buy";
-        m[MessageType::sell] = "sell";
-        m[MessageType::join_contract] = "join_contract";
-        m[MessageType::joining_contract] = "joining_contract";
-        m[MessageType::sign_refund] = "sign_refund";
-        m[MessageType::refund_signed] = "refund_signed";
-        m[MessageType::ready] = "ready";
-        m[MessageType::request_full_piece] = "request_full_piece";
-        m[MessageType::full_piece] = "full_piece";
-        m[MessageType::payment] = "payment";
-
-
-        // Return mapping
-        return m;
-    }
-
-    const char * messageName(MessageType type) {
-
-        // Get mapping
-        const std::map<MessageType, const char *> m = messageTypeToNameMapping();
-
-        // Find name of type
-        std::map<MessageType, const char *>::const_iterator i = m.find(type);
-
-        if(i == m.end())
-            throw std::runtime_error("No name registered for type, messageTypeToNameMapping() does not contain all types!");
+        if(messageName(MessageType::observe) == name)
+            return MessageType::observe;
+        else if(messageName(MessageType::buy) == name)
+            return MessageType::buy;
+        else if(messageName(MessageType::sell) == name)
+            return MessageType::sell;
+        else if(messageName(MessageType::join_contract) == name)
+            return MessageType::join_contract;
+        else if(messageName(MessageType::joining_contract) == name)
+            return MessageType::joining_contract;
+        else if(messageName(MessageType::sign_refund) == name)
+            return MessageType::sign_refund;
+        else if(messageName(MessageType::refund_signed) == name)
+            return MessageType::refund_signed;
+        else if(messageName(MessageType::ready) == name)
+            return MessageType::ready;
+        else if(messageName(MessageType::request_full_piece) == name)
+            return MessageType::request_full_piece;
+        else if(messageName(MessageType::full_piece) == name)
+            return MessageType::full_piece;
+        else if(messageName(MessageType::payment) == name)
+            return MessageType::payment;
         else
-            return i->second;
+            throw std::runtime_error("Invalid");
     }
-
-    MessageType messageType(const std::string & messageName) {
-
-        // Invert messageTypeToNameMapping()
-
-        // Get type to name mapping
-        const std::map<MessageType, const char *> m = messageTypeToNameMapping();
-
-        // Iterate map and find preimage of messageName
-        for(std::map<MessageType, const char *>::const_iterator i = m.begin(),
-                end(m.end()); i != end;i++) {
-
-            // return type if name matches
-            if(messageName.compare(i->second) == 0)
-                return i->first;
-        }
-
-        throw std::runtime_error("Unknown message name passed.");
-    }
-
-    const std::set<MessageType> allMessageTypes() {
-
-        // Create set
-        std::set<MessageType> m;
-
-        // Populate set by taking keys from messageTypeToNameMapping()
-        std::map<MessageType, const char *> x = messageTypeToNameMapping();
-
-        // Iterate map and find preimage of messageName
-        for(std::map<MessageType, const char *>::const_iterator i = x.begin(),
-                end(x.end()); i != end;i++)
-            m.insert(i->first);
-
-        // Return mappings
-        return m;
-    }
-
-    int numberOfMessageTypes() {
-        return allMessageTypes().size();
-    }
-
 }
 }
