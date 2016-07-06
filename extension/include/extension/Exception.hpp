89ad93a785a046c7006594c4e39569739ef648b6
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-06 14:02:54 +0200

    additional exceptions

--- extension/include/extension/Exception.hpp
+++ extension/include/extension/Exception.hpp
@@ -8,21 +8,163 @@
 #ifndef JOYSTREAM_EXTENSION_EXCEPTION_HPP
 #define JOYSTREAM_EXTENSION_EXCEPTION_HPP
 
+#include <protocol_wire/protocol_wire.hpp>
+#include <libtorrent/bdecode.hpp>
+
 #include <exception>
 
 namespace joystream {
 namespace extension {
 namespace exception {
 
-    class MissingTorrent : public std::runtime_error {
-
-    public:
+    struct MissingTorrent : public std::runtime_error {
 
         MissingTorrent()
             : std::runtime_error("Torrent missing") {
         }
+    };
+
+    // Thrown in writeMappingToMDictionary if a given message already has a mapping in the dictionary
+    struct MessageAlreadyPresentException : std::runtime_error {
+
+        MessageAlreadyPresentException(protocol_wire::MessageType message)
+            : std::runtime_error(toMessage(message))
+            , message(message) {
+        }
+
+        protocol_wire::MessageType message;
+
+    private:
+
+        static std::string toMessage(protocol_wire::MessageType message) {
+            return std::string("Provided dictionary already had key for message: ") + protocol_wire::messageName(message);
+        }
+    };
+
+    // Thrown in fromMDictionary if a message maps to a non-integer type
+
+    struct InvalidMessageMappingDictionary : std::runtime_error {
+
+        enum class Problem {
+            MessageMapsToInvalidValueTypeException,
+            MessageIdNegativeException,
+            IncompleteMessageSet,
+            ZeroIdInvalidOnNonUninstallMapping,
+            UninstallMappingFound,
+            NoSuchIdException,
+            InvalidMinimumMessageIdException
+        };
+
+        InvalidMessageMappingDictionary(Problem problem)
+            : std::runtime_error("Mapping dictionary was invalid.") // make problem spesific?
+            , problem(problem) {
+        }
+
+        Problem problem;
+    };
+
+/**
+    struct MessageMapsToInvalidValueTypeException : std::runtime_error {
+
+        // Subset of libtorrent::bdecode_node::type_t which excludes int_t
+        enum class FoundType {
+            none_t, // a dictionary node. The ``dict_find_`` functions are valid.
+            dict_t, // a list node. The ``list_`` functions are valid.
+            list_t, // a string node, the ``string_`` functions are valid.
+            string_t
+        };
+
+        static FoundType toFound(libtorrent::bdecode_node::type_t valueType) {
+
+            switch(valueType) {
+
+                case libtorrent::bdecode_node::none_t: return MessageMapsToInvalidValueTypeException::FoundType::none_t;
+                case libtorrent::bdecode_node::dict_t: return MessageMapsToInvalidValueTypeException::FoundType::dict_t;
+                case libtorrent::bdecode_node::list_t: return MessageMapsToInvalidValueTypeException::FoundType::list_t;
+                case libtorrent::bdecode_node::string_t: return MessageMapsToInvalidValueTypeException::FoundType::string_t;
+                case libtorrent::bdecode_node::int_t: throw std::runtime_error("Should not have libtorrent::bdecode_node::int_t.");
+
+                default:
+                    assert(false);
+            }
+
+        }
+
+        MessageMapsToInvalidValueTypeException(protocol_wire::MessageType message, FoundType valueType)
+            : std::runtime_error(toMessage(message, valueType))
+            , message(message)
+            , valueType(valueType) {
+        }
+
+        protocol_wire::MessageType message;
+        FoundType valueType;
+
+    private:
+
+        static std::string toMessage(protocol_wire::MessageType message, FoundType valueType) {
+            return std::string("Message ") + protocol_wire::messageName(message) + " maps to non-integer type " + stringFrom(valueType);
+        }
+
+        static const char * stringFrom(FoundType type) {
+
+            switch(type) {
+                case FoundType::none_t: return "none";
+                case FoundType::dict_t: return "dictionary";
+                case FoundType::list_t: return "list";
+                case FoundType::string_t: return "string";
+                default:
+                    assert(false);
+            }
+        }
+    };
+
+    struct MessageIdNegativeException : std::runtime_error {
+
+        MessageIdNegativeException(protocol_wire::MessageType message, boost::int64_t value)
+            : std::runtime_error("")
+            , message(message)
+            , value(value) {
+        }
+
+        protocol_wire::MessageType message;
+        boost::int64_t value;
+    };
+
+    struct IncompleteMessageSet : std::runtime_error {
+
+        IncompleteMessageSet()
+            : std::runtime_error("Not all messages had a key.") {
+        }
+    };
+
+    struct ZeroIdInvalidOnNonUninstallMapping : std::runtime_error {
+
+        ZeroIdInvalidOnNonUninstallMapping()
+            : std::runtime_error("A 0 id found, despite mapping not being uninstall.") {
+        }
+    };
+
+    struct UninstallMappingFound : std::runtime_error {
+
+        UninstallMappingFound()
+            : std::runtime_error("Uninstall mapping found, all messages are found, but mapping to 0.") {
+        }
+    };
+
+    struct NoSuchIdException : std::runtime_error {
+
+        NoSuchIdException()
+            : std::runtime_error("No message with such id found.") {}
 
     };
+*/
+
+    struct InvalidMinimumMessageIdException : std::runtime_error {
+
+        InvalidMinimumMessageIdException()
+            : std::runtime_error("The minimum message id must be non-zero positive integer.") {}
+    };
+
 }
 }
 }
