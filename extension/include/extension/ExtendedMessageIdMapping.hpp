967ec313b5b3405e8228c85540db4ac4aedf2eeb
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-06 13:52:46 +0200

    simplified and extended ExtendedMessageIdMapping
    * support uninstalling mapping detection
    * use new MessageTypes
    * exceptions for decoding failures

--- extension/include/extension/ExtendedMessageIdMapping.hpp
+++ extension/include/extension/ExtendedMessageIdMapping.hpp
@@ -9,62 +9,72 @@
 #define JOYSTREAM_EXTENSION_EXTENDED_MESSAGE_ID_MAPPING_HPP
 
 #include <protocol_wire/protocol_wire.hpp>
-
+#include <libtorrent/bdecode.hpp>
 #include <libtorrent/entry.hpp>
 
-#include <QtGlobal> // quint8
+#include <set>
+#include <map>
+#include <exception>
+
+#define CLIENT_PREFIX_STRING "js_"
 
 namespace joystream {
-
-namespace protocol_wire {
-    enum class MessageType;
-}
-
 namespace extension {
 
+    // A BEP10 handshake message id mapping for the crypto currency extension.
+    // Mapping is always bijective (one-to-one + onto)
     class ExtendedMessageIdMapping {
 
     public:
 
-        // Default constructor
-        ExtendedMessageIdMapping();
+        ExtendedMessageIdMapping(const ExtendedMessageIdMapping &);
 
-        // Constructor from extended hanshake dictionary m, used when parsing hanshake from peer
-        ExtendedMessageIdMapping(const libtorrent::bdecode_node & m);
-
-        // Assignment operator
         ExtendedMessageIdMapping & operator=(const ExtendedMessageIdMapping & rhs);
 
-        // Outputs mapping into and m dicitonary, which can be used in extended handshake
-        void writeToMDictionary(std::map<std::string, libtorrent::entry> & m);
-
-        // Get extended message id of given message
-        quint8 id(joystream::protocol_wire::MessageType messageType) const;
-
-        // Set extended message id of given message
-        void id(joystream::protocol_wire::MessageType messageType, quint8 id);
-
-        // Get message corresponding to id
-        joystream::protocol_wire::MessageType messageType(quint8 id) const;
-
-        // Check that mapping is valid
-        bool isValid() const;
+        // Constructor from extended hanshake dictionary m.
+        // Throws: exception::InvalidMessageMappingDictionary
+        static ExtendedMessageIdMapping fromMDictionary(const libtorrent::bdecode_node & m);
 
         // Sets all message ids starting at the given parameter
-        void setAllStartingAt(quint8 _id);
+        static ExtendedMessageIdMapping consecutiveIdsStartingAt(uint8_t start);
 
-        // Get underlying std::map
-        std::map<joystream::protocol_wire::MessageType, quint8> mapping() const;
+        // Writes an uninstall mapping into (BEP10) m dictionary
+        // Throws:
+        // * MessageAlreadyPresentException: if dictionary already has key corresponding to this extension
+        static void writeUninstallMappingToMDictionary(libtorrent::entry::dictionary_type &);
 
-        // Set underlying std::map
-        void setMapping(const std::map<joystream::protocol_wire::MessageType, quint8> & mapping);
+        // Writes this mapping into (BEP10) m dictionary
+        // Throws:
+        // * MessageAlreadyPresentException: if dictionary already has key corresponding to this extension
+        void writeToMDictionary(libtorrent::entry::dictionary_type & m);
+
+        /// Getters
+
+        // Returns id of given message
+        uint8_t id(protocol_wire::MessageType) const;
+
+        // Returns the message with the givne id
+        // Throws:
+        // NoSuchIdException: if there is no message with given id
+        protocol_wire::MessageType messageType(uint8_t) const;
 
     private:
 
-        // Message to id mapping
-        std::map<joystream::protocol_wire::MessageType, quint8> _mapping;
-    };
+        typedef std::map<joystream::protocol_wire::MessageType, uint8_t> RawMapping;
 
+        // Write raw mapping to m dictionary entry
+        static void writeMappingToMDictionary(const RawMapping &, libtorrent::entry::dictionary_type &);
+
+        // Private default
+        ExtendedMessageIdMapping(const RawMapping &);
+
+        // Full set of messages used in ma
+        const static std::set<protocol_wire::MessageType> messages;
+
+        // Message to id mapping
+        // Invariant: _mapping.size() == messages.size()
+        RawMapping _mapping;
+    };
 }
 }
 
