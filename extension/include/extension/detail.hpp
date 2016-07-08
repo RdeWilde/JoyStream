e26257087f68a66c33b051d7ae638b38eb4ec509
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-08 08:44:53 +0200

    new(detail): moved variant and visitor into separate filesa and tidied up

---
+++ extension/include/extension/detail.hpp
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, July 7 2016
+ */
+
+#ifndef JOYSTREAM_EXTENSION_DETAIL_HPP
+#define JOYSTREAM_EXTENSION_DETAIL_HPP
+
+#include <extension/Alert.hpp>
+#include <extension/Request.hpp>
+#include <boost/variant.hpp>
+#include <exception>
+#include <functional>
+
+namespace joystream {
+namespace extension {
+
+class Plugin;
+
+namespace detail {
+
+// Variant used to allow a single request queue
+typedef boost::variant<request::Start,
+                       request::Stop,
+                       request::Pause,
+                       request::UpdateBuyerTerms,
+                       request::UpdateSellerTerms,
+                       request::ToObserveMode,
+                       request::ToSellMode,
+                       request::ToBuyMode,
+                       request::UpdateStatus> RequestVariant;
+
+class RequestVariantVisitor : public boost::static_visitor<> {
+
+public:
+
+    RequestVariantVisitor(Plugin * plugin)
+        : _plugin(plugin) {
+    }
+
+    void operator()(const request::Start & r);
+    void operator()(const request::Stop & r);
+    void operator()(const request::Pause & r);
+    void operator()(const request::UpdateBuyerTerms & r);
+    void operator()(const request::UpdateSellerTerms & r);
+    void operator()(const request::ToObserveMode & r);
+    void operator()(const request::ToSellMode & r);
+    void operator()(const request::ToBuyMode & r);
+    void operator()(const request::UpdateStatus & r);
+
+private:
+
+    //
+    void sendRequestResult(const alert::LoadedCallback &);
+
+    //
+    std::exception_ptr runTorrentPluginRequest(const libtorrent::sha1_hash &,
+                                               const std::function<void(const boost::shared_ptr<TorrentPlugin> &)> & f) const;
+
+    Plugin * _plugin;
+};
+
+}
+}
+}
+
+
+#endif // JOYSTREAM_EXTENSION_DETAIL_HPP
---
+++ extension/src/detail.cpp
@@ -0,0 +1,134 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, July 7 2016
+ */
+
+#include <extension/detail.hpp>
+#include <extension/Alert.hpp>
+#include <extension/Exception.hpp>
+#include <extension/Plugin.hpp>
+
+namespace joystream {
+namespace extension {
+namespace detail {
+
+void RequestVariantVisitor::operator()(const request::Start & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->start();
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::Stop & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->stop();
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::Pause & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->pause();
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::UpdateBuyerTerms & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [r](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->updateTerms(r.terms);
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::UpdateSellerTerms & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [r](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->updateTerms(r.terms);
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::ToObserveMode & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [r](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->toObserveMode();
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::ToSellMode & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [r](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->toSellMode(r.generateKeyPairsCallbackHandler,
+                           r.generateP2PKHAddressesCallbackHandler,
+                           r.sellingPolicy,
+                           r.terms);
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::ToBuyMode & r) {
+
+    auto e = runTorrentPluginRequest(r.infoHash, [r](const boost::shared_ptr<TorrentPlugin> & plugin) {
+        plugin->toBuyMode(r.generateKeyPairsCallbackHandler,
+                          r.generateP2PKHAddressesCallbackHandler,
+                          r.funding,
+                          r.policy,
+                          r.terms);
+    });
+
+    sendRequestResult(std::bind(r.handler, e));
+}
+
+void RequestVariantVisitor::operator()(const request::UpdateStatus &) {
+    _plugin->_session->alerts().emplace_alert<alert::PluginStatus>(_plugin->status());
+}
+
+std::exception_ptr RequestVariantVisitor::runTorrentPluginRequest(const libtorrent::sha1_hash & infoHash,
+                                                                  const std::function<void(const boost::shared_ptr<TorrentPlugin> &)> & f) const {
+
+    // Make sure there is a torrent plugin for this torrent
+    auto it = _plugin->_plugins.find(infoHash);
+
+    std::exception_ptr e;
+
+    // If there is no torrent plugin, then tell client
+    if(it == _plugin->_plugins.cend())
+        e = std::make_exception_ptr(exception::MissingTorrent());
+    else {
+
+        // Make sure the plugin is still valid
+        boost::shared_ptr<TorrentPlugin> plugin = it->second.lock();
+
+        assert(plugin);
+
+        try {
+            f(plugin);
+        } catch (...) {
+            e = std::current_exception();
+        }
+    }
+
+    return e;
+}
+
+void RequestVariantVisitor::sendRequestResult(const alert::LoadedCallback & c) {
+    _plugin->_session->alerts().emplace_alert<alert::RequestResult>(c);
+}
+
+}
+}
+}
