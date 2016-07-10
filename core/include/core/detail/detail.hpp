f2e18085aa8527a85c70cd57fb31f03fb679b213
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-10 19:37:42 +0200

    drop(core::detail::Torrent): dropping all

--- core/include/core/detail/detail.hpp
+++ core/include/core/detail/detail.hpp
@@ -16,135 +16,7 @@
 
 namespace joystream {
 namespace core {
-
-class Node;
-class Torrent;
-
 namespace detail {
-
-struct Torrent {
-
-    enum class State {
-
-        // Torrent has been libtorrent::async_add_torrent has been called, without response
-        waiting_for_async_added,
-
-        // Torrent was successfully added to session.
-        // Can be set either from successfull libtorrent::add_torrent_alert,
-        // or synchronous adding which succeeds.
-        waiting_for_torrent_to_be_checked,
-
-        // When a torrent is added, some checking is done
-        // - primarily resume data I think, and when that is completed
-        // the libtorrent::torrent_checked_alert alert is issued by libtorrent
-        // torrent_checked,
-
-        // <== this should probably be removed
-        normal,
-
-        // Result of some pending resume_data() call,
-        // can be either
-        // libtorrent::save_resume_data_alert
-        // libtorrent::save_resume_data_failed_alert
-        waiting_for_resume_data
-    };
-
-    Torrent(extension::Plugin * plugin,
-            const libtorrent::sha1_hash & infoHash,
-            const std::string & name,
-            const std::string & savePath,
-            const std::vector<char> & resumeData,
-            std::uint64_t flags,
-            State state);
-
-    /// Plugin actions
-
-    void start(const extension::request::SubroutineHandler &);
-
-    void stop(const extension::request::SubroutineHandler &);
-
-    void pause(const extension::request::SubroutineHandler &);
-
-    void updateTerms(const protocol_wire::BuyerTerms & terms,
-                     const extension::request::SubroutineHandler &);
-
-    void updateTerms(const protocol_wire::SellerTerms & terms,
-                     const extension::request::SubroutineHandler &);
-
-    void toObserveMode(const extension::request::SubroutineHandler &);
-
-    void toSellMode(const protocol_session::GenerateKeyPairsCallbackHandler & generateKeyPairsCallbackHandler,
-                    const protocol_session::GenerateP2PKHAddressesCallbackHandler & generateP2PKHAddressesCallbackHandler,
-                    const protocol_session::SellingPolicy & sellingPolicy,
-                    const protocol_wire::SellerTerms & terms,
-                    const extension::request::SubroutineHandler &);
-
-    void toBuyMode(const protocol_session::GenerateKeyPairsCallbackHandler & generateKeyPairsCallbackHandler,
-                   const protocol_session::GenerateP2PKHAddressesCallbackHandler & generateP2PKHAddressesCallbackHandler,
-                   const Coin::UnspentP2PKHOutput & funding,
-                   const protocol_session::BuyingPolicy & policy,
-                   const protocol_wire::BuyerTerms & terms,
-                   const extension::request::SubroutineHandler &);
-
-    /**
-    // Stream management
-    void addStream(Stream * stream);
-    void removeStream(Stream * stream);
-
-    // Given piece was read
-    void pieceRead(const boost::shared_array<char> & buffer, int pieceIndex, int size);
-
-    // Given piece was downloaded and checked
-    void pieceFinished(int piece);
-    */
-
-    /// Members
-
-    // Plugin
-    extension::Plugin * plugin;
-
-    // Info hash of torrent
-    libtorrent::sha1_hash infoHash;
-
-    // Display name
-    std::string name;
-
-    // Save path
-    std::string savePath;
-
-    // Resume data
-    std::vector<char> resumeData;
-
-    // Flags
-    std::uint64_t flags;
-
-    /**
-    // Handle to torrent
-    // A valid handle is only set after the torrent has been added
-    // successfully to session
-    libtorrent::torrent_handle _handle;
-    */
-
-    // Status
-    State state;
-
-    // Add const pointer to const object of type TorrentPlugin in the future?
-    // can be used to look at stuff like plugin mode etc.
-    // worth looking at.
-
-    // Public interface of torrent
-    // We opt for shared pointer, rather than by value,
-    // so that client can be given corresponding weak_ptr.
-    // Giving a raw pointer can allow client to access object
-    // after it is deleted. The controller::removedTorrent signal helps avoid
-    // that, but it is not perfect, as client cannot be guaranteed to see signal in time.
-    std::shared_ptr<core::Torrent> model;
-
-    // All streams for this torrent.
-    // Not quite sure if multiple separate streams for one torrent
-    // is necessary, if not, then remove this QSet later.
-    //QSet<Stream *> _streams;
-};
 
 // An unsynchronized counter shared across multiple callbacks.
 class SharedCounter {
@@ -170,7 +42,7 @@ public:
         return (*ptr);
     }
 
-    bool decrement() const {
+    void decrement() const {
 
         uint * ptr = _count.get();
 
@@ -178,12 +50,14 @@ public:
             throw std::runtime_error("Counter already depleted.");
         else
             (*ptr)--;
-
-        return done();
     }
 
-    bool done() const {
-        return *(_count.get()) == 0;
+    uint count() const {
+        return *(_count.get());
+    }
+
+    bool zero() const {
+        return count() == 0;
     }
 
 private:
