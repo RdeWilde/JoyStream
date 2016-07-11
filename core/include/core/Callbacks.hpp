2d4ef49c9cd31af97d29f2828c8828ce29c0eb80
Bedeho Mender <bedeho.mender@gmail.com>
2016-07-11 18:30:24 +0200

    new(Callbacks): moved all Node callbacks into separate file

---
+++ core/include/core/Callbacks.hpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, July 11 2016
+ */
+
+#ifndef JOYSTREAM_CORE_CALLBACKS_HPP
+#define JOYSTREAM_CORE_CALLBACKS_HPP
+
+#include <libtorrent/socket.hpp>
+#include <libtorrent/error_code.hpp>
+#include <functional>
+
+namespace joystream {
+namespace core {
+
+typedef std::function<void(const libtorrent::tcp::endpoint &)> NodeStarted;
+typedef std::function<void(const libtorrent::tcp::endpoint &, libtorrent::error_code)> NodeStartFailed;
+
+typedef std::function<void()> NodeStopped;
+
+typedef std::function<void(const libtorrent::error_code)> AddedTorrent;
+
+}
+}
+
+#endif // JOYSTREAM_CORE_CALLBACKS_HPP
