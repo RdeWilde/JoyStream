76211a836b62ab18be2ceb18a6e1aa1f5c34093b
Bedeho Mender <bedeho.mender@gmail.com>
2016-08-19 14:56:42 +0200

    fix(core::Language): straggeling inital commit
    Annoying Qt auto staging causes this

--- classic/gui/include/gui/Language.hpp
+++ classic/gui/include/gui/Language.hpp
@@ -1,5 +1,36 @@
-#ifndef LANGUAGE
-#define LANGUAGE
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, August 19 2016
+ */
 
-#endif // LANGUAGE
+#ifndef JOYSTREAM_CLASSIC_GUI_LANGUAGE_HPP
+#define JOYSTREAM_CLASSIC_GUI_LANGUAGE_HPP
+
+#include <libtorrent/torrent_status.hpp>
+
+#include <QString>
+
+namespace joystream {
+namespace protocol_session {
+    enum class SessionMode;
+}
+namespace classic {
+namespace gui {
+
+class Language {
+
+public:
+
+    static QString toString(const libtorrent::torrent_status::state_t & state, float progress);
+    static QString toString(protocol_session::SessionMode mode);
+
+};
+
+}
+}
+}
+
+#endif // JOYSTREAM_CLASSIC_GUI_LANGUAGE_HPP
 
--- classic/gui/src/Language.cpp
+++ classic/gui/src/Language.cpp
@@ -1,1 +1,86 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, August 19 2016
+ */
 
+#include <gui/Language.hpp>
+
+#include <protocol_session/protocol_session.hpp>
+
+#include <libtorrent/torrent_status.hpp>
+
+#include <QObject>
+
+namespace joystream {
+namespace classic {
+namespace gui {
+
+QString Language::toString(const libtorrent::torrent_status::state_t & state, float progress) {
+
+    switch(state) {
+
+        #ifndef TORRENT_NO_DEPRECATE
+
+        case libtorrent::torrent_status::queued_for_checking:
+
+                return QObject::tr("Queued for checking");
+
+        #else
+
+        case libtorrent::torrent_status::unused_enum_for_backwards_compatibility:
+
+                assert(false);
+                return QObject::tr("Internal unused state");
+
+        #endif
+
+        case libtorrent::torrent_status::checking_files:
+
+                return QObject::tr("Checking files") + QString::number(progress*100,'f',0).append("%");
+
+        case libtorrent::torrent_status::downloading_metadata:
+
+                return QObject::tr("Downloading metadata") + " " + QString::number(progress*100,'f',0).append("%");
+
+        case libtorrent::torrent_status::downloading:
+
+                return QObject::tr("Downloading") + " " + QString::number(progress*100,'f',0).append("%");
+
+        case libtorrent::torrent_status::finished:
+
+                return QObject::tr("Finished");
+
+        case libtorrent::torrent_status::seeding:
+
+                return QObject::tr("Seeding");
+
+        case libtorrent::torrent_status::allocating:
+
+                return QObject::tr("Allocating") + " " + QString::number(progress*100,'f',0).append("%");
+
+        case libtorrent::torrent_status::checking_resume_data:
+
+                return QObject::tr("Checking resume data") + " " + QString::number(progress*100,'f',0).append("%");
+
+        default:
+                assert(false);
+    }
+}
+
+QString Language::toString(protocol_session::SessionMode mode) {
+
+    switch(mode) {
+        case protocol_session::SessionMode::buying: return QObject::tr("Buying");
+        case protocol_session::SessionMode::not_set: return QObject::tr("Not set");
+        case protocol_session::SessionMode::observing: return QObject::tr("Observing");
+        case protocol_session::SessionMode::selling: return QObject::tr("Selling");
+        default:
+        assert(false);
+    }
+}
+
+}
+}
+}
