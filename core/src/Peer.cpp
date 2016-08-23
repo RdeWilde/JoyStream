439a13ed38bf48171c02b33472e45cd4d665af9d
Bedeho Mender <bedeho.mender@gmail.com>
2016-08-23 20:47:10 +0200

    fix(core::Peer): implemented and added a few more getters

--- core/src/Peer.cpp
+++ core/src/Peer.cpp
@@ -14,6 +14,30 @@ Peer::Peer(const libtorrent::peer_info & peerInformation)
     : _peerInformation(peerInformation) {
 }
 
+libtorrent::tcp::endpoint Peer::endPoint() const noexcept {
+    return _peerInformation.ip;
+}
+
+std::string Peer::client() {
+    return _peerInformation.client;
+}
+
+boost::int64_t Peer::total_download() const noexcept {
+    return _peerInformation.total_download;
+}
+
+boost::int64_t Peer::total_upload() const noexcept {
+    return _peerInformation.total_upload;
+}
+
+int Peer::payload_up_speed() const noexcept {
+    return _peerInformation.payload_up_speed;
+}
+
+int Peer::payload_down_speed() const noexcept {
+    return _peerInformation.payload_down_speed;
+}
+
 void Peer::update(const libtorrent::peer_info & peerInformation) {
 
     if(peerInformation.total_download != _peerInformation.total_download)
