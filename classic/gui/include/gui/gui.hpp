b69369d8af697745924a415326b7c083e22de048
Bedeho Mender <bedeho.mender@gmail.com>
2016-09-14 17:51:44 +0200

    new(gui.hpp): added convenience header

---
+++ classic/gui/include/gui/gui.hpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) JoyStream - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bedeho Mender <bedeho.mender@gmail.com>, June 26 2015
+ */
+
+#ifndef JOYSTREAM_CLASSIC_GUI_HPP
+#define JOYSTREAM_CLASSIC_GUI_HPP
+
+#include <gui/BuyerTermsDialog.hpp>
+#include <gui/BuyingModeSessionWidget.hpp>
+#include <gui/CurrencyFieldPrefixSettings.hpp>
+#include <gui/FundingWalletProgressDialog.hpp>
+#include <gui/GeneralLoadingProgressDialog.hpp>
+#include <gui/Language.hpp>
+#include <gui/MainWindow.hpp>
+#include <gui/PeersDialog.hpp>
+#include <gui/ReceiveFundsDialog.hpp>
+#include <gui/SelectTorrentForDownloadDialog.hpp>
+#include <gui/SellerTermsDialog.hpp>
+#include <gui/SellingModeSessionWidget.hpp>
+#include <gui/SessionDialog.hpp>
+#include <gui/TorrentContextMenu.hpp>
+#include <gui/TorrentTreeViewRow.hpp>
+#include <gui/TrayMenuManager.hpp>
+
+#endif // JOYSTREAM_CLASSIC_GUI_HPP
