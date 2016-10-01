39c08ff2a26c079f3e37c17c9ebba8f19d72e653
Bedeho Mender <bedeho.mender@gmail.com>
2016-10-01 09:12:35 +0200

    update(gui.hpp)

--- classic/gui/include/gui/gui.hpp
+++ classic/gui/include/gui/gui.hpp
@@ -9,21 +9,26 @@
 #define JOYSTREAM_CLASSIC_GUI_HPP
 
 #include <gui/BuyerTermsDialog.hpp>
-#include <gui/BuyingModeSessionWidget.hpp>
+#include <gui/SellerTermsDialog.hpp>
+#include <gui/SessionDialog/SellingModeSessionWidget.hpp>
+#include <gui/SessionDialog/SessionDialog.hpp>
+#include <gui/SessionDialog/BuyingModeSessionWidget.hpp>
+#include <gui/PeersDialog/PeersDialog.hpp>
+#include <gui/PeersDialog/PeerTreeViewRow.hpp>
+#include <gui/PeersDialog/BuyerConnectionTreeViewRow.hpp>
+#include <gui/PeersDialog/ObserverConnectionTreeViewRow.hpp>
+#include <gui/PeersDialog/SellerConnectionTreeViewRow.hpp>
+#include <gui/MainWindow/MainWindow.hpp>
+#include <gui/MainWindow/TorrentContextMenu.hpp>
+#include <gui/MainWindow/TorrentTableRowModel.hpp>
+#include <gui/MainWindow/TorrentTableModel.hpp>
+
+#include <gui/Common.hpp>
+#include <gui/TrayMenuManager.hpp>
+#include <gui/ReceiveFundsDialog.hpp>
+#include <gui/SelectTorrentForDownloadDialog.hpp>
 #include <gui/CurrencyFieldPrefixSettings.hpp>
 #include <gui/FundingWalletProgressDialog.hpp>
 #include <gui/GeneralLoadingProgressDialog.hpp>
-#include <gui/Common.hpp>
-#include <gui/PeersDialog/PeersDialog.hpp>
-#include <gui/PeersDialog/PeerTreeViewRow.hpp>
-#include <gui/ReceiveFundsDialog.hpp>
-#include <gui/SelectTorrentForDownloadDialog.hpp>
-#include <gui/SellerTermsDialog.hpp>
-#include <gui/SellingModeSessionWidget.hpp>
-#include <gui/SessionDialog.hpp>
-#include <gui/MainWindow/MainWindow.hpp>
-#include <gui/MainWindow/TorrentContextMenu.hpp>
-#include <gui/MainWindow/TorrentTreeViewRow.hpp>
-#include <gui/TrayMenuManager.hpp>
 
 #endif // JOYSTREAM_CLASSIC_GUI_HPP
