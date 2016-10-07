224f1c2502e914bb4dda63d41d4960a9eb827f7c
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-10-07 23:30:42 +0300

    appkit_cli - minor update

--- test/app_kit_cli/main.cpp
+++ test/app_kit_cli/main.cpp
@@ -49,16 +49,28 @@ uint32_t future_unix_timestamp(const unsigned int minutes) {
     return std::chrono::duration_cast<std::chrono::seconds>(delta).count();
 }
 
+std::string stateToString(libtorrent::torrent_status::state_t state) {
+    switch (state) {
+        case libtorrent::torrent_status::state_t::allocating: return "allocating";
+        case libtorrent::torrent_status::state_t::checking_files: return "checking_files";
+        case libtorrent::torrent_status::state_t::checking_resume_data: return "checking_resume_data";
+        case libtorrent::torrent_status::state_t::downloading: return "downloading";
+        case libtorrent::torrent_status::state_t::downloading_metadata: return "downloading_metadata";
+        case libtorrent::torrent_status::state_t::finished: return "finished";
+        case libtorrent::torrent_status::state_t::seeding: return "seeding";
+    }
 
+    throw std::runtime_error("invalid state");
+}
 
 int main(int argc, char *argv[])
 {
     const int nsellers = 1;
-    const uint64_t price = 100; //satoshis per piece
+    const uint64_t price = 10; //satoshis per piece
     const uint32_t locktime = future_unix_timestamp(5); // 5 minute locktime
-    const uint64_t refund_fee = 20000;
-    const uint64_t settlement_fee = 20000;
-    const uint64_t contractFeeRate = 20000;   //satoshis/KByte - ref https://bitcoinfees.github.io/
+    const uint64_t refund_fee = 0; // refund fee is remeniscent of old protocol is it still needed?
+    const uint64_t settlement_fee = 0;
+    const uint64_t contractFeeRate = 20;   //satoshis/KByte - ref https://bitcoinfees.github.io/
 
     joystream::protocol_wire::BuyerTerms buyerTerms(price, locktime, nsellers, contractFeeRate, refund_fee);
     joystream::protocol_wire::SellerTerms sellerTerms(price, locktime, nsellers, contractFeeRate, settlement_fee);
@@ -119,7 +131,9 @@ int main(int argc, char *argv[])
 
     QTimer *timer = new QTimer();
     QObject::connect(timer, &QTimer::timeout, [&timer, &app, &kit](){
-        if(!shuttingDown) return;
+        if(!shuttingDown) {
+            return;
+        }
 
         timer->stop();
 
@@ -134,60 +148,93 @@ int main(int argc, char *argv[])
     signal(SIGINT, &handleSignal);
     signal(SIGTERM, &handleSignal);
 
-    auto startIt = [](const std::exception_ptr &eptr, joystream::core::Torrent* torrent) {
-        if(eptr){
-            std::rethrow_exception(eptr);
-        }else {
-            torrent->torrentPlugin()->start([](const std::exception_ptr &eptr){
-                if(eptr){
-                   std::rethrow_exception(eptr);
-                }else {
-                    std::cout << "Torrent Started" << std::endl;
-                }
-            });
-        }
+    auto startIt = [](joystream::core::Torrent* torrent) {
+        torrent->torrentPlugin()->start([](const std::exception_ptr &eptr){
+            if(eptr){
+               std::rethrow_exception(eptr);
+            }else {
+                std::cout << "Torrent Started" << std::endl;
+            }
+        });
     };
 
     auto buyIt = [&kit, &buyerTerms, &buyingPolicy, &startIt](joystream::core::Torrent* torrent) {
         kit->buyTorrent(torrent, buyingPolicy, buyerTerms, [torrent, &startIt](const std::exception_ptr &eptr){
-            startIt(eptr, torrent);
+            if(eptr){
+                // unable to go to buy mode, try again in 5 seconds..?
+            } else {
+                startIt(torrent);
+            }
         });
     };
 
     auto sellIt = [&kit, &sellerTerms, &startIt](joystream::core::Torrent* torrent) {
         kit->sellTorrent(torrent, joystream::protocol_session::SellingPolicy(), sellerTerms, [torrent, &startIt](const std::exception_ptr &eptr){
-            startIt(eptr, torrent);
-        });
-    };
-
-    auto onPluginAdded = [&buyIt, &sellIt, &argv](joystream::core::Torrent* torrent) {
-        // wait for torrent pluging to be added before we can go to buy/sell mode...
-        QObject::connect(torrent, &joystream::core::Torrent::torrentPluginAdded, [&buyIt, &sellIt, &argv, torrent](const joystream::core::TorrentPlugin *plugin){
-            // assuming this signal will only ever be emitted once
-            if(std::string(argv[1]) == "buy") {
-                buyIt(torrent);
+            if(eptr){
+                // unable to go to sell mode, try again in 5 seconds..?
             } else {
-                sellIt(torrent);
+                startIt(torrent);
             }
         });
     };
+
+    // wait for torrent to be added
+    QObject::connect(kit->node(), &joystream::core::Node::addedTorrent, [&buyIt, &sellIt, &argv](joystream::core::Torrent * torrent){
+        std::cout << "Torrent Added Successfully" << std::endl;
+
+        // wait for torrent plugin to be added before we can go to buy/sell mode...
+        QObject::connect(torrent, &joystream::core::Torrent::torrentPluginAdded, [&buyIt, &sellIt, &argv, torrent](const joystream::core::TorrentPlugin *plugin){
+            std::cout << "Torrent Plugin Added Successfully" << std::endl;
+            std::cout << "Torrent State: " << stateToString(torrent->state()) << std::endl;
+
+            // ready to download?
+            if(libtorrent::torrent_status::state_t::downloading == torrent->state()
+                    && std::string(argv[1]) == "buy") {
+                buyIt(torrent);
+                return;
+            }
+
+            // ready to upload?
+            if(libtorrent::torrent_status::state_t::seeding == torrent->state()
+                    && std::string(argv[1]) == "sell") {
+                sellIt(torrent);
+                return;
+            }
+
+            // otherwise.. wait for valid state
+            QObject::connect(torrent, &joystream::core::Torrent::stateChanged, [&buyIt, &sellIt, &argv, torrent](libtorrent::torrent_status::state_t state, float progress){
+                std::cout << ">>>>>>>>>> Torrent State changed to: " << stateToString(state) << std::endl;
+
+                if(libtorrent::torrent_status::state_t::downloading == state
+                        && std::string(argv[1]) == "buy") {
+                    buyIt(torrent);
+                    return;
+                }
+
+                if(libtorrent::torrent_status::state_t::seeding == state
+                        && std::string(argv[1]) == "sell") {
+                    sellIt(torrent);
+                    return;
+                }
+            });
+
+        });
+    });
 
     if(torrentIdentifier) {
         std::cout << "Adding Torrent" << std::endl;
 
         auto savePath = (dataDirectory + QDir::separator() + "downloads").toStdString();
-
+        // if torrent is added paused, it doesn't get unpaused in torrentPlugin::start()
         kit->node()->addTorrent(0, 0, "test", std::vector<char>(), savePath, false, *torrentIdentifier,
-                               [&kit, &onPluginAdded](libtorrent::error_code &ecode, libtorrent::torrent_handle &th){
+                               [](libtorrent::error_code &ecode, libtorrent::torrent_handle &th){
+
             if(ecode) {
-                std::cerr << "Node::addTorrent() failed: " << ecode.message().c_str() << std::endl;
-                return;
+                std::cerr << "addTorrent failed: " << ecode.message().c_str() << std::endl;
             }
 
-            // make sure to have metadata before gonig to buy or sell torrent
-            auto torrent = kit->node()->torrents()[th.info_hash()];
+            std::cout << "Torrent Starting Status:" << stateToString(th.status().state) << std::endl;
 
-            onPluginAdded(torrent);
         });
     }
 
