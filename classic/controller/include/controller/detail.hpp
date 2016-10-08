8d4c5a42b85233d3d4a370f78f2fc418b06e66ff
Bedeho Mender <bedeho.mender@gmail.com>
2016-10-08 19:46:55 +0200

    new(classic::controller::detail)
    * new PeerDialogModelManager

--- classic/controller/include/controller/detail.hpp
+++ classic/controller/include/controller/detail.hpp
@@ -8,10 +8,16 @@
 #ifndef JOYSTREAM_CLASSIC_CONTROLLER_DETAIL_HPP
 #define JOYSTREAM_CLASSIC_CONTROLLER_DETAIL_HPP
 
+#include <libtorrent/socket.hpp>
+
 #include <boost/variant.hpp>
 #include <boost/optional.hpp>
 
 namespace joystream {
+namespace protocol_wire {
+    class SellerTerms;
+    class BuyerTerms;
+}
 namespace classic {
 namespace gui {
     class PeerTableRowModel;
@@ -25,12 +31,27 @@ namespace gui {
 namespace controller {
 namespace detail {
 
-struct PeerDialogModels {
+struct PeerDialogModelManager {
 
-    PeerDialogModels(gui::PeerTableRowModel * classicTableRowModel,
-                     gui::BuyerTableModel * buyerTableModel,
-                     gui::ObserverTableModel * observerTableModel,
-                     gui::SellerTableModel * sellerTableModel);
+    PeerDialogModelManager(const libtorrent::tcp::endpoint & endPoint,
+                           gui::PeerTableRowModel * classicTableRowModel,
+                           gui::BuyerTableModel * buyerTableModel,
+                           gui::ObserverTableModel * observerTableModel,
+                           gui::SellerTableModel * sellerTableModel);
+
+    /// Mode announcement handlers
+
+    void noneModeAnnounced();
+
+    void observeModeAnnounced();
+
+    void sellModeAnnounced(const protocol_wire::SellerTerms & terms);
+
+    void buyModeAnnounced(const protocol_wire::BuyerTerms & terms);
+
+    /// Members
+
+    libtorrent::tcp::endpoint _endPoint;
 
     // Model for classic view of this peer
     gui::PeerTableRowModel * _classicTableRowModel;
--- classic/controller/src/detail.cpp
+++ classic/controller/src/detail.cpp
@@ -7,6 +7,9 @@
 
 #include <controller/detail.hpp>
 #include <gui/gui.hpp>
+#include <protocol_wire/protocol_wire.hpp>
+
+#include <boost/variant/get.hpp>
 
 namespace joystream {
 namespace classic {
@@ -15,16 +18,96 @@ namespace detail {
 
 /// PeerDialogModels
 
-PeerDialogModels::PeerDialogModels(gui::PeerTableRowModel * classicTableRowModel,
-                                   gui::BuyerTableModel * buyerTableModel,
-                                   gui::ObserverTableModel * observerTableModel,
-                                   gui::SellerTableModel * sellerTableModel)
-    : _classicTableRowModel(classicTableRowModel)
+PeerDialogModelManager::PeerDialogModelManager(const libtorrent::tcp::endpoint & endPoint,
+                                               gui::PeerTableRowModel * classicTableRowModel,
+                                               gui::BuyerTableModel * buyerTableModel,
+                                               gui::ObserverTableModel * observerTableModel,
+                                               gui::SellerTableModel * sellerTableModel)
+    : _endPoint(endPoint)
+    , _classicTableRowModel(classicTableRowModel)
     , _buyerTableModel(buyerTableModel)
     , _observerTableModel(observerTableModel)
     , _sellerTableModel(sellerTableModel) {
 }
 
+void PeerDialogModelManager::noneModeAnnounced() {
+
+    if(_modeAnnouncedRowModel.is_initialized()) {
+
+        detail::PeerDialogModelManager::ModeAnnouncedRowModel activeRow = _modeAnnouncedRowModel.get();
+
+        switch(activeRow.which()) {
+
+            case 0: _buyerTableModel->remove(boost::get<gui::BuyerTableRowModel *>(activeRow)->row()); break;
+            case 1: _observerTableModel->remove(boost::get<gui::ObserverTableRowModel *>(activeRow)->row()); break;
+            case 2: _sellerTableModel->remove(boost::get<gui::SellerTableRowModel *>(activeRow)->row()); break;
+
+            default:
+                assert(false);
+        }
+
+    }
+}
+
+void PeerDialogModelManager::observeModeAnnounced() {
+
+    if(_modeAnnouncedRowModel.is_initialized()) {
+
+        detail::PeerDialogModelManager::ModeAnnouncedRowModel activeRow = _modeAnnouncedRowModel.get();
+
+        switch(activeRow.which()) {
+
+            case 0: _buyerTableModel->remove(boost::get<gui::BuyerTableRowModel *>(activeRow)->row()); break;
+            case 1: break; // nothing to do, already in observe mode
+            case 2: _sellerTableModel->remove(boost::get<gui::SellerTableRowModel *>(activeRow)->row()); break;
+
+            default:
+                assert(false);
+        }
+
+    } else
+        _modeAnnouncedRowModel = _observerTableModel->add(_endPoint);
+}
+
+void PeerDialogModelManager::sellModeAnnounced(const protocol_wire::SellerTerms & terms) {
+
+    if(_modeAnnouncedRowModel.is_initialized()) {
+
+        detail::PeerDialogModelManager::ModeAnnouncedRowModel activeRow = _modeAnnouncedRowModel.get();
+
+        switch(activeRow.which()) {
+
+            case 0: _buyerTableModel->remove(boost::get<gui::BuyerTableRowModel *>(activeRow)->row()); break;
+            case 1: _observerTableModel->remove(boost::get<gui::ObserverTableRowModel *>(activeRow)->row()); break;
+            case 2: boost::get<gui::SellerTableRowModel *>(activeRow)->setTerms(terms); break;
+
+            default:
+                assert(false);
+        }
+
+    } else
+        _modeAnnouncedRowModel = _sellerTableModel->add(_endPoint);
+}
+
+void PeerDialogModelManager::buyModeAnnounced(const protocol_wire::BuyerTerms & terms) {
+
+    if(_modeAnnouncedRowModel.is_initialized()) {
+
+        detail::PeerDialogModelManager::ModeAnnouncedRowModel activeRow = _modeAnnouncedRowModel.get();
+
+        switch(activeRow.which()) {
+
+            case 0: boost::get<gui::BuyerTableRowModel *>(activeRow)->setTerms(terms); break;
+            case 1: _observerTableModel->remove(boost::get<gui::ObserverTableRowModel *>(activeRow)->row()); break;
+            case 2: _sellerTableModel->remove(boost::get<gui::SellerTableRowModel *>(activeRow)->row()); break;
+
+            default:
+                assert(false);
+        }
+
+    } else
+        _modeAnnouncedRowModel = _buyerTableModel->add(_endPoint);
+}
 
 }
 }
