294e6fae45614611985d21e83d6ce263d5c844cb
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-10-19 08:10:35 +0300

    tests - paymentchannel regtest - remove spvwallet

--- test/paymentchannel_regtest/Test.cpp
+++ test/paymentchannel_regtest/Test.cpp
@@ -8,17 +8,12 @@
 
 #include "bitcoin_regtest_framework.hpp"
 
-#include <bitcoin/SPVWallet.hpp>
-#include <bitcoin/Common.hpp>
-
-#include <common/Seed.hpp>
 #include <common/PrivateKey.hpp>
 #include <common/PublicKey.hpp>
 #include <common/P2PKHAddress.hpp>
 #include <common/UnspentOutput.hpp>
 #include <common/UnspentOutputSet.hpp>
 #include <common/Utilities.hpp>
-#include <common/Entropy.hpp>
 
 #include <CoinCore/hdkeys.h>
 #include <CoinCore/CoinNodeData.h> // Coin::Transaction
@@ -33,31 +28,14 @@ using namespace joystream::test::bitcoin;
 void Test::initTestCase() {
 
     QVERIFY(regtest::init() == 0);
-
-    // SPVWallet - CoinQ netsync log
-    if(getenv("NETSYNC_LOGGER") != NULL) {
-        INIT_LOGGER("sync.log");
-    }
 }
 
 void Test::init() {
-    // delete existing wallet files
-    boost::filesystem::remove(TEST_WALLET_PATH_A);
-    boost::filesystem::remove(TEST_WALLET_PATH_B);
 
-    // delete existing blocktree file
-    boost::filesystem::remove(TEST_BLOCKTREE_PATH_A);
-    boost::filesystem::remove(TEST_BLOCKTREE_PATH_B);
-
-    // create new testnet wallet
-    _walletA = new joystream::bitcoin::SPVWallet(TEST_WALLET_PATH_A, TEST_BLOCKTREE_PATH_A, Coin::Network::regtest);
-    _walletB = new joystream::bitcoin::SPVWallet(TEST_WALLET_PATH_B, TEST_BLOCKTREE_PATH_B, Coin::Network::regtest);
 }
 
 void Test::cleanup() {
-    _walletA->stopSync();
-    delete _walletA;
-    delete _walletB;
+
 }
 
 void Test::cleanupTestCase() {
--- test/paymentchannel_regtest/Test.hpp
+++ test/paymentchannel_regtest/Test.hpp
@@ -8,26 +8,12 @@
 #define TEST_HPP
 
 #include <QtTest/QtTest>
-namespace joystream {
-namespace bitcoin {
-    class SPVWallet;
-}
-}
-
-#define WALLET_SEED Coin::Seed("27891465891239001238391236589203948574567842549230457167823941893047812940123194312489312840923849010124893128409238490101248931")
-#define TEST_BITCOIN_NETWORK Coin::Network::regtest
-#define TEST_WALLET_PATH_A "test-wallet_1.db"
-#define TEST_BLOCKTREE_PATH_A "test-blocktree_1.dat"
-#define TEST_WALLET_PATH_B "test-wallet_2.db"
-#define TEST_BLOCKTREE_PATH_B "test-blocktree_2.dat"
 
 class Test : public QObject
 {
     Q_OBJECT
 
 private:
-    joystream::bitcoin::SPVWallet *_walletA;
-    joystream::bitcoin::SPVWallet *_walletB;
 
 private slots:
 
