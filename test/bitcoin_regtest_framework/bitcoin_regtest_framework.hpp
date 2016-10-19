c3a0f356de972ad6e48a7f832bd6d221269b701c
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-10-19 16:33:22 +0300

    bitcoin_regtest_framework - handle multiline outputs from rpc commands, add support for additional commands

--- test/bitcoin_regtest_framework/bitcoin_regtest_framework.hpp
+++ test/bitcoin_regtest_framework/bitcoin_regtest_framework.hpp
@@ -25,15 +25,34 @@ int purge_data_dir() {
     return system("rm -fr bitcoind-test/");
 }
 
-BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int rpc(const std::string & command) {
+std::string make_cli_command(const std::string & command) {
     std::string sys_string = "bitcoin-cli -regtest -rpcuser=test -rpcpassword=test ";
-    std::string full_commandline = sys_string + command;
-
-    return system(full_commandline.c_str());
+    return sys_string + command;
 }
 
-BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int rpc(const std::stringstream & command) {
-    return rpc(command.str());
+int rpc(const std::string &command) {
+    return system(make_cli_command(command).c_str());
+}
+
+int rpc_output(std::string command, std::string & result) {
+    FILE *output = popen(make_cli_command(command).c_str(), "r");
+
+    if (!output) {
+        return errno;
+    }
+
+    char buffer[64000];
+    char *line_p;
+    while((line_p = fgets(buffer, sizeof(buffer), output))) {
+        result += buffer;
+    }
+
+    // strip off trailing new line character
+    if(result.back() == '\n') {
+        result.resize(result.size() - 1);
+    }
+
+    return pclose(output);
 }
 
 int wait_for_ready() {
@@ -44,7 +63,7 @@ BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int generate_blocks(const uint & number_o
     std::stringstream command;
     command << "generate ";
     command << number_of_blocks;
-    return rpc(command);
+    return rpc(command.str());
 }
 
 BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int send_to_address(const std::string &addr, const std::string &amount) {
@@ -52,7 +71,54 @@ BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int send_to_address(const std::string &ad
     command << "sendtoaddress ";
     command << addr << " ";
     command << amount;
-    return rpc(command);
+    return rpc(command.str());
+}
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int send_to_address(const std::string &addr, const std::string &amount, std::string &txid) {
+    std::stringstream command;
+    command << "sendtoaddress ";
+    command << addr << " ";
+    command << amount;
+    return rpc_output(command.str(), txid);
+}
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int send_raw_transaction(const std::string &rawSignedTransaction) {
+    return rpc("sendrawtransaction " + rawSignedTransaction);
+}
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT std::string getnewaddress() {
+    std::string newAddress;
+    int err = rpc_output("getnewaddress", newAddress);
+
+    if(err)
+        throw std::runtime_error(std::string("bitcoin: unable to generate a new address") + std::string(strerror(err)));
+
+    return newAddress;
+}
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT std::string getPrivKey(std::string base58EncodedAddress) {
+    std::string privKey;
+    int err = rpc_output("dumpprivkey " + base58EncodedAddress, privKey);
+
+    if(err)
+        throw std::runtime_error(std::string("bitcoin: unable to get private key") + std::string(strerror(err)));
+
+    return privKey;
+}
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT int importPrivKey(std::string privKey) {
+    return rpc("importprivkey " + privKey);
+}
+
+
+BITCOIN_REGTEST_FRAMEWORKSHARED_EXPORT std::string listunspent() {
+    std::string list;
+    int err = rpc_output("listunspent 0", list);
+
+    if(err)
+        throw std::runtime_error(std::string("bitcoin: unable to get list of unspent outputs") + std::string(strerror(err)));
+
+    return list;
 }
 
 int start_node() {
