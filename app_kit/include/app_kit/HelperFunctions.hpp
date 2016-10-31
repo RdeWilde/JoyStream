0f61fd55834940d4fa52ee73230712446c739b5b
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2016-10-31 18:04:04 +0200

    appkit - use helper functions for serializing/deserializing buyer/seller terms and policy

--- app_kit/include/app_kit/HelperFunctions.hpp
+++ app_kit/include/app_kit/HelperFunctions.hpp
@@ -3,6 +3,13 @@
 
 #include <libtorrent/sha1_hash.hpp>
 #include <core/TorrentIdentifier.hpp>
+#include <protocol_session/SessionState.hpp>
+#include <protocol_session/BuyingPolicy.hpp>
+#include <protocol_session/SellingPolicy.hpp>
+#include <protocol_wire/BuyerTerms.hpp>
+#include <protocol_wire/SellerTerms.hpp>
+
+#include <QJsonValue>
 
 namespace joystream{
 namespace appkit {
@@ -10,7 +17,23 @@ namespace util {
 
 // Utility functions
 libtorrent::sha1_hash sha1_hash_from_hex_string(const char *);
-core::TorrentIdentifier* makeTorrentIdentifier(const char *);
+core::TorrentIdentifier* makeTorrentIdentifier(const std::string arg);
+
+
+QJsonValue sessionStateToJson(const protocol_session::SessionState&);
+protocol_session::SessionState jsonToSessionState(const QJsonValue&);
+
+QJsonValue buyerTermsToJson(const protocol_wire::BuyerTerms&);
+protocol_wire::BuyerTerms jsonToBuyerTerms(const QJsonValue&);
+
+QJsonValue buyingPolicyToJson(const protocol_session::BuyingPolicy&);
+protocol_session::BuyingPolicy jsonToBuyingPolicy(const QJsonValue&);
+
+QJsonValue sellerTermsToJson(const protocol_wire::SellerTerms&);
+protocol_wire::SellerTerms jsonToSellerTerms(const QJsonValue&);
+
+QJsonValue sellingPolicyToJson(const protocol_session::SellingPolicy&);
+protocol_session::SellingPolicy jsonToSellingPolicy(const QJsonValue&);
 
 }
 }
