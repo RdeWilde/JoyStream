a0e7e342457b553f87c6499021d989a2452e173e
Lola Rigaut-Luczak <rllola80@gmail.com>
2016-11-07 08:58:43 +0100

    Verify torrent_identifier not null

--- daemon/RPCAddTorrent.cpp
+++ daemon/RPCAddTorrent.cpp
@@ -8,33 +8,39 @@ RPCAddTorrent::RPCAddTorrent(joystream::daemon::rpc::Daemon::AsyncService* servi
 
 void RPCAddTorrent::onCall()
 {
+    // Pop up a new instance for concurency
+    new RPCAddTorrent(service_, cq_, node_);
+
     std::string save_path = std::string("/home/lola/joystream");
     std::vector<char> resume_data = std::vector<char>();
     std::string name = std::string(request_.name());
     boost::optional<uint> upload_limit = -1;
     boost::optional<uint> download_limit = -1;
     bool paused = 1;
-    joystream::core::TorrentIdentifier torrent_identifier = joystream::core::TorrentIdentifier::fromHashString(std::string(request_.infohash()));
+    joystream::core::TorrentIdentifier* torrent_identifier = joystream::core::TorrentIdentifier::fromHashString(std::string(request_.infohash()));
 
     std::cout << "We are adding the torrent" << std::endl;
 
-    // Pop up a new instance for concurency
-    new RPCAddTorrent(service_, cq_, node_);
-
-    try {
-        node_->addTorrent(upload_limit,download_limit,name,resume_data,save_path,paused,torrent_identifier,[this](libtorrent::error_code &ecode, libtorrent::torrent_handle &th) {
-            if (ecode) {
-                std::cout << "We have an error" << std::endl;
-                this->responder_.Finish(this->request_, grpc::Status::CANCELLED, this);
-                this->status_ = FINISH;
-            } else {
-                std::cout << "New torrent added" << std::endl;
-                this->responder_.Finish(this->request_, grpc::Status::OK, this);
-                this->status_ = FINISH;
-            }
-        });
-    } catch (joystream::core::exception::TorrentAlreadyExists error) {
-        std::cout << "Some kind of errors" << std::endl;
+    // if torrent_identifier not nullptr it means that hash was correct
+    if (torrent_identifier != nullptr) {
+        try {
+            node_->addTorrent(upload_limit,download_limit,name,resume_data,save_path,paused,torrent_identifier,[this](libtorrent::error_code &ecode, libtorrent::torrent_handle &th) {
+                if (ecode) {
+                    std::cout << "We have an error" << std::endl;
+                    this->responder_.Finish(this->request_, grpc::Status::CANCELLED, this);
+                    this->status_ = FINISH;
+                } else {
+                    std::cout << "New torrent added" << std::endl;
+                    this->responder_.Finish(this->request_, grpc::Status::OK, this);
+                    this->status_ = FINISH;
+                }
+            });
+        } catch (joystream::core::exception::TorrentAlreadyExists error) {
+            std::cout << "Some kind of errors" << std::endl;
+            this->responder_.Finish(this->request_, grpc::Status::CANCELLED, this);
+            this->status_ = FINISH;
+        }
+    } else {
         this->responder_.Finish(this->request_, grpc::Status::CANCELLED, this);
         this->status_ = FINISH;
     }
