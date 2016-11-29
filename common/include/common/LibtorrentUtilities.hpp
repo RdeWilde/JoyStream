7837edb732cf06b64ff71fb2b7e60343002d35d0
Bedeho Mender <bedeho.mender@gmail.com>
2016-11-29 21:53:46 +0100

    new(common::hash<libtorrent::sha1_hash>)

--- common/include/common/LibtorrentUtilities.hpp
+++ common/include/common/LibtorrentUtilities.hpp
@@ -14,6 +14,21 @@ unsigned int qHash(const libtorrent::tcp::endpoint & endpoint);
 //#include <libtorrent/peer_connection.hpp> // libtorrent::peer_request
 //uint qHash(const libtorrent::peer_request & request);
 
+#include <libtorrent/sha1_hash.hpp>
+
+// custom specialization of std::hash can be injected in namespace std
+namespace std
+{
+    template<> struct hash<libtorrent::sha1_hash>
+    {
+        typedef libtorrent::sha1_hash argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& s) const
+        {
+            return std::hash<std::string>{}(s.to_string());
+        }
+   };
+}
 
 #endif // LIBTORRENT_UTILITIES_HPP
 
