77f41e31907dd1bf134c043a48da6da7c4190b32
Bedeho Mender <bedeho.mender@gmail.com>
2017-01-21 20:25:05 +0100

    Added new macroes
    - some from fanatid
    - one to object decoding

--- poc/bindings/cpp/utils.hpp
+++ poc/bindings/cpp/utils.hpp
@@ -134,4 +134,88 @@ std::string To(const v8::Local<v8::Value> & val);
 // @return {std::string}
 #define GET_STD_STRING(o, key)    (To<std::string>(GET_VAL(o, key)))
 
+///////////////////////////////////////////////////////////////////////////////
+/// Utility macros from @fanatid in macro.hpp@a4901b
+///////////////////////////////////////////////////////////////////////////////
+
+#define REQUIRE_ARGUMENTS(n)                                                  \
+  if (info.Length() < (n)) {                                                  \
+    return Nan::ThrowTypeError("Expected " #n "arguments");                   \
+  }
+
+#define ARGUMENTS_OPTIONAL_OBJECT(i, var, defaultValue)                       \
+  v8::Local<v8::Object> var;                                                  \
+  if (info.Length() > i && !info[i]->IsUndefined()) {                         \
+    if (!info[i]->IsObject()) {                                               \
+      return Nan::ThrowTypeError("Argument " #i " must be a Object");         \
+    }                                                                         \
+    var = info[i]->ToObject();                                                \
+  } else {                                                                    \
+    var = (defaultValue);                                                     \
+  }
+
+#define ARGUMENTS_IS_NUMBER(i)                                                \
+  (info.Length() > (i) && info[i]->IsNumber())
+
+#define ARGUMENTS_REQUIRE_NUMBER(i, var)                                      \
+  if (!ARGUMENTS_IS_NUMBER(i)) {                                              \
+    return Nan::ThrowTypeError("Argument " #i " must be a Number");           \
+  }                                                                           \
+  int64_t var = info[i]->IntegerValue();
+
+#define ARGUMENTS_IS_BOOLEAN(i)                                               \
+  (info.Length() > (i) && info[i]->IsBoolean())
+
+#define ARGUMENTS_REQUIRE_BOOLEAN(i, var)                                     \
+  if (!ARGUMENTS_IS_BOOLEAN(i)) {                                             \
+    return Nan::ThrowTypeError("Argument " #i " must be a Boolean");          \
+  }                                                                           \
+  bool var = info[i]->BooleanValue();
+
+#define ARGUMENTS_IS_STRING(i)                                                \
+  (info.Length() > (i) && info[i]->IsString())
+
+#define ARGUMENTS_REQUIRE_STRING(i, var)                                      \
+  if (!ARGUMENTS_IS_STRING(i)) {                                              \
+    return Nan::ThrowTypeError("Argument " #i " must be a String");           \
+  }                                                                           \
+  Nan::Utf8String var(info[i]);
+
+#define ARGUMENTS_IS_FUNCTION(i)                                              \
+  (info.Length() > (i) && info[i]->IsFunction())
+
+#define ARGUMENTS_REQUIRE_FUNCTION(i, var)                                    \
+  if (!ARGUMENTS_IS_FUNCTION(i)) {                                            \
+    return Nan::ThrowTypeError("Argument " #i " must be a Function");         \
+  }                                                                           \
+  v8::Local<v8::Function> var = v8::Local<v8::Function>::Cast(info[i]);
+
+#define ARGUMENTS_IS_OBJECT(i)                                               \
+  (info.Length() >= (i) && info[i]->IsObject())
+
+#define ARGUMENTS_IS_INSTANCE(i, cls)                                         \
+  (ARGUMENTS_IS_OBJECT(i) && Nan::New(cls::prototype)->HasInstance(info[i]))
+
+#define ARGUMENTS_REQUIRE_INSTANCE(i, cls, var)                               \
+  if (!ARGUMENTS_IS_INSTANCE(i, cls)) {                                       \
+    return Nan::ThrowTypeError("Argument " #i " must be a " #cls);            \
+  }                                                                           \
+  cls* var = Nan::ObjectWrap::Unwrap<cls>(info[i]->ToObject());
+
+///////////////////////////////////
+
+// Recover object wrapped on this object
+#define THIS(cls)                                                             \
+  Nan::ObjectWrap::Unwrap<cls>(info.This())                                   \
+
+// Recover type from object encoding
+#define ARGUMENTS_REQUIRE_OBJECT(i, type, var)                                \
+  type var;                                                                   \
+  try {                                                                       \
+    var = fromValue(info[i]);                                                 \
+  } catch (const std::runtime_error & e) {                                    \
+    std::string m("Argument " #i " is not a " #type " object: ");             \
+    return Nan::ThrowTypeError(m + e.what());                                 \
+  }
+
 #endif
