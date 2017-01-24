56414afbe6ea9acda86125c20492de3e0bb80e3e
Bedeho Mender <bedeho.mender@gmail.com>
2017-01-24 12:32:04 +0100

    Updating plugin binding interface
    - use new alert decoder

--- poc/bindings/cpp/plugin.hpp
+++ poc/bindings/cpp/plugin.hpp
@@ -5,7 +5,12 @@
  * Written by Bedeho Mender <bedeho.mender@gmail.com>, January 21 2017
  */
 
+ #ifndef JOYSTREAM_NODE_PLUGIN_HPP
+ #define JOYSTREAM_NODE_PLUGIN_HPP
+
 #include <nan.h>
+
+#include "common.hpp"
 
 #include <boost/shared_ptr.hpp>
 
@@ -23,17 +28,9 @@ class plugin : public Nan::ObjectWrap {
 public:
 
   /**
-   * @brief Alert converter for this plugin.
-   * @param a alert for conversion.
-   * @param o object to which alert should be converted.
-   * @param success will be written to to indicate whether the alert was recognized, and written to ´o´.
-   */
-  typedef void(*converter)(const libtorrent::alert *, v8::Local<v8::Object> & o, bool success);
-
-  /**
    * @brief Get alert converter for this plugin.
    */
-  virtual converter getConverter() const noexcept = 0;
+  virtual AlertDecoder getDecoder() const noexcept = 0;
 
   /**
    * @brief Returns reference to this plugin.
@@ -44,3 +41,5 @@ public:
 
 }
 }
+
+#endif // JOYSTREAM_NODE_PLUGIN_HPP
